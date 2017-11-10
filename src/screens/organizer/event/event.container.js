edfb9135fbb5b3102ba850f281262a0d4dbd8dd5
Benjamin Petetot <bpetetot@gmail.com>
2017-11-10 17:20:37 +0100

    create event in firestore

--- src/screens/organizer/event/event.container.js
+++ src/screens/organizer/event/event.container.js
@@ -1,15 +1,14 @@
 import { connect } from 'react-redux'
 
+import { saveEvent } from '../../../redux/event'
 import Event from './event'
 
 const mapState = () => ({
   initialValues: { name: 'test' },
 })
 
-const mapDispatch = () => ({
-  onSubmit: (values) => {
-    console.log(values)
-  },
+const mapDispatch = dispatch => ({
+  onSubmit: event => dispatch(saveEvent(event)),
 })
 
 export default connect(mapState, mapDispatch)(Event)
