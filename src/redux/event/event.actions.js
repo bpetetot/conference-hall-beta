5fb8bbfa49eb594da1f72e4f7f6ee54c69599c43
Benjamin Petetot <bpetetot@gmail.com>
2017-11-10 22:47:09 +0100

    get event with id in database

--- src/redux/event/event.actions.js
+++ src/redux/event/event.actions.js
@@ -17,6 +17,24 @@ export const saveEvent = event => async (dispatch, getState, fb) => {
     // go to event page
     dispatch(push(`/organizer/event/${ref.id}`))
   } catch (e) {
-    console.log(e)
+    console.error(e)
+  }
+}
+
+export const getEvent = id => async (dispatch, getState, fb) => {
+  try {
+    // get event from database
+    const ref = await fb
+      .firestore()
+      .collection('events')
+      .get(id)
+
+    if (ref.exists) {
+      console.log(ref.data())
+    } else {
+      console.warn(`event with id ${id} not found`)
+    }
+  } catch (e) {
+    console.error(e)
   }
 }
