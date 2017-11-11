136cd52f14a728d75f138aa25737a0e1ea033afb
Benjamin Petetot <bpetetot@gmail.com>
2017-11-11 18:55:42 +0100

    add maps in event info

--- src/screens/organizer/event/info/eventInfo.jsx
+++ src/screens/organizer/event/info/eventInfo.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import EventMaps from './eventMaps'
+import EventMap from '../../../../components/maps'
+import IconLabel from '../../../../components/iconLabel'
+import './eventInfo.css'
 
 const EventInfo = ({
   name, description, address, website,
 }) => (
-  <div>
-    <h1>{name}</h1>
-    <small>{website}</small>
-    <p>{description}</p>
-    <address>{address}</address>
-    <EventMaps address={address} />
+  <div className="event-info">
+    <div className="event-data">
+      <h1>{name}</h1>
+      {website && (
+        <p>
+          <a href={website} target="NEW">
+            <IconLabel icon="fa fa-globe" label="Website" />
+          </a>
+        </p>
+      )}
+      {address && (
+        <p>
+          <a href={`https://www.google.com/maps/place/${encodeURI(address)}`} target="NEW">
+            <IconLabel icon="fa fa-map-marker" label={address} />
+          </a>
+        </p>
+      )}
+      <p>{description}</p>
+    </div>
+    <div className="event-map">
+      <EventMap address={address} />
+    </div>
   </div>
 )
 
