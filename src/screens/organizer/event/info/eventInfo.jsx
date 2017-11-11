bdec5d8fd2938e97a556766b0b8fcfb96902a557
Benjamin Petetot <bpetetot@gmail.com>
2017-11-11 23:29:48 +0100

    :sparkles: display markdown in html (#5)

--- src/screens/organizer/event/info/eventInfo.jsx
+++ src/screens/organizer/event/info/eventInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactMarkdown from 'react-markdown'
 
 import EventMap from '../../../../components/maps'
 import IconLabel from '../../../../components/iconLabel'
@@ -9,27 +10,31 @@ const EventInfo = ({
   name, description, address, website,
 }) => (
   <div className="event-info">
-    <div className="event-data">
+    <div className="event-header">
       <h1>{name}</h1>
       {website && (
-        <p>
+        <div>
           <a href={website} target="NEW">
             <IconLabel icon="fa fa-globe" label="Website" />
           </a>
-        </p>
+        </div>
       )}
       {address && (
-        <p>
+        <div>
           <a href={`https://www.google.com/maps/place/${encodeURI(address)}`} target="NEW">
             <IconLabel icon="fa fa-map-marker" label={address} />
           </a>
-        </p>
+        </div>
       )}
-      <p>{description}</p>
     </div>
-    <div className="event-map">
-      <EventMap address={address} />
+    <div className="event-content">
+      <ReactMarkdown className="markdown" source={description} escapeHtml />
     </div>
+    {address && (
+      <div className="event-map">
+        <EventMap address={address} />
+      </div>
+    )}
   </div>
 )
 
