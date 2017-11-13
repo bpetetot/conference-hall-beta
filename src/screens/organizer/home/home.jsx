b3fa5cf5bce5f554b5e0940b70551dc5dcc22643
Benjamin Petetot <bpetetot@gmail.com>
2017-11-13 20:52:04 +0100

    remove testing toast button

--- src/screens/organizer/home/home.jsx
+++ src/screens/organizer/home/home.jsx
@@ -4,14 +4,11 @@ import { Link } from 'redux-little-router'
 import IconLabel from 'components/iconLabel'
 import './home.css'
 
-const Home = ({ toast }) => (
+const Home = () => (
   <div className="organizer-home">
     <Link href="/organizer/event/create" className="btn btn-link">
       <IconLabel icon="fa fa-calendar-plus-o" label="Create event" />
     </Link>
-    <button className="btn btn-primary" onClick={() => toast(Date.now())}>
-      Toast
-    </button>
   </div>
 )
 
