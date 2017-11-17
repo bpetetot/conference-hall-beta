3e25ffe726ab446c5e6e7d65ae47f60c790cbda4
Benjamin Petetot <bpetetot@gmail.com>
2017-11-17 17:49:14 +0100

    set routing

--- src/screens/organizer/event/form/eventForm.jsx
+++ src/screens/organizer/event/form/eventForm.jsx
@@ -4,34 +4,38 @@ import { Field, reduxForm } from 'redux-form'
 
 import { input, address, textarea, radio, SubmitButton, required } from 'components/form'
 import RadioGroup from '../../../../components/form/radioGroup'
+import EventTabs from './eventTabs'
 import './eventForm.css'
 
 const EventForm = ({
   form, handleSubmit, pristine, submitting, invalid, type,
 }) => (
-  <form onSubmit={handleSubmit} className="event-form">
-    <RadioGroup name="type" inline>
-      <Field name="type" value="conference" label="Conference" type="radio" component={radio} />
-      <Field name="type" value="meetup" label="Meetup" type="radio" component={radio} />
-    </RadioGroup>
-    <Field name="name" label="Name" type="text" component={input} validate={required} autoFocus />
-    <Field name="description" label="description" component={textarea} validate={required} />
-    <Field
-      name="address"
-      label={type === 'conference' ? 'Venue address' : 'City'}
-      type="text"
-      component={address}
-    />
-    <Field name="website" label="Website" type="text" component={input} />
-    <Field name="tags" label="Tags" type="text" component={input} />
-    <SubmitButton
-      disabled={pristine || submitting || invalid}
-      submitting={submitting}
-      loadingMessage="Saving..."
-    >
-      {form === 'event-create' ? 'Create event' : 'Update event'}
-    </SubmitButton>
-  </form>
+  <div>
+    <EventTabs />
+    <form onSubmit={handleSubmit} className="event-form">
+      <RadioGroup name="type" inline>
+        <Field name="type" value="conference" label="Conference" type="radio" component={radio} />
+        <Field name="type" value="meetup" label="Meetup" type="radio" component={radio} />
+      </RadioGroup>
+      <Field name="name" label="Name" type="text" component={input} validate={required} autoFocus />
+      <Field name="description" label="description" component={textarea} validate={required} />
+      <Field
+        name="address"
+        label={type === 'conference' ? 'Venue address' : 'City'}
+        type="text"
+        component={address}
+      />
+      <Field name="website" label="Website" type="text" component={input} />
+      <Field name="tags" label="Tags" type="text" component={input} />
+      <SubmitButton
+        disabled={pristine || submitting || invalid}
+        submitting={submitting}
+        loadingMessage="Saving..."
+      >
+        {form === 'event-create' ? 'Create event' : 'Update event'}
+      </SubmitButton>
+    </form>
+  </div>
 )
 
 EventForm.propTypes = {
---
+++ src/screens/organizer/event/form/eventTabs/eventTabs.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'redux-little-router'
+
+import IconLabel from '../../../../../components/iconLabel'
+import './eventTabs.css'
+
+const EventTabs = ({ id }) => (
+  <div className="event-tabs">
+    <ul className="tabs">
+      <li>
+        <Link href={`/organizer/event/${id}/edit`} activeProps={{ className: 'tab-active' }}>
+          <IconLabel icon="fa fa-calendar-check-o" label="Event information" />
+        </Link>
+      </li>
+      <li>
+        <Link href={`/organizer/event/${id}/edit/cfp`} activeProps={{ className: 'tab-active' }}>
+          <IconLabel icon="fa fa-gear" label="CFP Settings" />
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={`/organizer/event/${id}/edit/members`}
+          activeProps={{ className: 'tab-active' }}
+        >
+          <IconLabel icon="fa fa-users" label="Members" />
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+EventTabs.propTypes = {
+  id: PropTypes.string.isRequired,
+}
+
+export default EventTabs
