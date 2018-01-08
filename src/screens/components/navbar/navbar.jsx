84196e65d22afe319db05851a747822b82af6b37
Benjamin Petetot <bpetetot@gmail.com>
2018-01-08 20:45:41 +0100

    :sparkles: report a bug link

--- src/screens/components/navbar/navbar.jsx
+++ src/screens/components/navbar/navbar.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Navbar from 'components/navbar'
+import IconLink from 'components/iconLink'
 import AvatarDropdown from './avatarDropdown'
+
+import './navbar.css'
 
 const SpeakerNavbar = ({ className }) => (
   <Navbar className={className}>
+    <IconLink
+      icon="fa fa-github"
+      label="report a bug"
+      className="navbar-link"
+      href="https://github.com/bpetetot/conference-hall/issues/new"
+    />
     <AvatarDropdown />
   </Navbar>
 )
