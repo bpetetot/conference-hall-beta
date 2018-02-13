7c998c717510f8528e07251786eb5b9f7e9a3596
Benjamin Petetot <bpetetot@gmail.com>
2018-02-13 20:20:33 +0100

    add event loading on proposal route

--- src/screens/organizer/proposals/proposals.container.js
+++ src/screens/organizer/proposals/proposals.container.js
@@ -2,12 +2,22 @@ import { compose } from 'redux'
 import { inject } from 'k-ramel/react'
 import forRoute from 'hoc-little-router'
 
+import loader from 'components/loader'
 import { getRouterParam } from 'store/reducers/router'
 import Proposals from './proposals'
 
-const mapStore = store => ({ eventId: getRouterParam('eventId')(store.getState()) })
+const mapStore = (store) => {
+  const eventId = getRouterParam('eventId')(store.getState())
+  const event = store.data.events.get(eventId)
+  return {
+    loaded: !!event,
+    eventId,
+    load: () => store.dispatch('@@ui/ON_LOAD_EVENT'),
+  }
+}
 
 export default compose(
   forRoute.absolute('PROPOSALS'), //
   inject(mapStore), //
+  loader, //
 )(Proposals)
