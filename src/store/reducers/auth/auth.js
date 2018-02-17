50c38cdd7346204db5c84d6fd078a18997e8b6d2
Benjamin Petetot <bpetetot@gmail.com>
2018-02-17 12:30:55 +0100

    :sparkles: new auth providers twitter, github and facebook

--- src/store/reducers/auth/auth.js
+++ src/store/reducers/auth/auth.js
@@ -1,6 +1,8 @@
 import { simpleObject } from 'k-ramel'
+import split from 'lodash/split'
 
 const defaultData = {
+  providers: split(process.env.REACT_APP_AUTH_PROVIDERS, ','),
   initialized: false,
   authenticated: false,
   uid: undefined,
