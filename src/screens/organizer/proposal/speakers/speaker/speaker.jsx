ad0f854e7346fadb7ea2f27dfc78ff357506afce
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-02-23 19:58:48 +0100

    :sparkles: Change IconLabel by IconLink

--- src/screens/organizer/proposal/speakers/speaker/speaker.jsx
+++ src/screens/organizer/proposal/speakers/speaker/speaker.jsx
@@ -3,25 +3,33 @@ import PropTypes from 'prop-types'
 
 import Avatar from 'screens/components/speaker'
 import IconLabel from 'components/iconLabel'
+import IconLink from 'components/iconLink'
 import Markdown from 'components/markdown'
 import './speaker.css'
 
 const Speaker = ({
   uid, email, github, twitter, company, language, city, bio,
-}) => (
-  <div>
-    <Avatar id={uid} className="proposal-speaker-avatar" />
-    <div className="proposal-speaker-icons">
-      <IconLabel icon="fa fa-envelope-o" label={email} />
-      <IconLabel icon="fa fa-github" label={github} />
-      <IconLabel icon="fa fa-twitter" label={twitter} />
-      <IconLabel icon="fa fa-building-o" label={company} />
-      <IconLabel icon="fa fa-map-marker" label={city} />
-      <IconLabel icon="fa fa-language" label={language} />
+}) => {
+  let twitterUrl
+  if (twitter) {
+    twitterUrl = twitter.startsWith('@') ? twitter.substr(1) : twitter
+  }
+
+  return (
+    <div>
+      <Avatar id={uid} className="proposal-speaker-avatar" />
+      <div className="proposal-speaker-icons">
+        <IconLink icon="fa fa-envelope-o" label={email} href={`mailto:${email}`} />
+        <IconLink icon="fa fa-github" label={github} href={`https://github.com/${github}`} />
+        <IconLink icon="fa fa-twitter" label={twitter} href={`https://twitter.com/${twitterUrl}`} />
+        <IconLabel icon="fa fa-building-o" label={company} />
+        <IconLabel icon="fa fa-map-marker" label={city} />
+        <IconLabel icon="fa fa-language" label={language} />
+      </div>
+      <Markdown source={bio} />
     </div>
-    <Markdown source={bio} />
-  </div>
-)
+  )
+}
 
 Speaker.propTypes = {
   uid: PropTypes.string.isRequired,
