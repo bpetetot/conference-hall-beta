a36aa6f2f7ee32c87544367f24bf6c58bcc94c31
Benjamin Petetot <bpetetot@gmail.com>
2018-02-23 10:56:28 +0100

    :bug: fix no event in sidebar

--- src/store/listeners/reactions/router.js
+++ src/store/listeners/reactions/router.js
@@ -1,7 +1,7 @@
 import { reaction } from 'k-ramel'
 import { initializeCurrentLocation } from 'redux-little-router'
 
-import { isRoute, getRouterParam } from 'store/reducers/router'
+import { isRoute, isSpeakerRoute, getRouterParam } from 'store/reducers/router'
 
 export const init = reaction((action, store) => {
   const initialLocation = store.getState().router
@@ -12,14 +12,26 @@ export const init = reaction((action, store) => {
 
 export const onRouteChanged = reaction((action, store) => {
   const state = store.getState()
+
+  // when speaker route get last eventId in localstorage
+  if (isSpeakerRoute(state) && !store.ui.app.get().currentEventId) {
+    const eventId = localStorage.getItem('currentEventId')
+    if (eventId) {
+      store.ui.app.update({ currentEventId: eventId })
+      store.dispatch({ type: '@@ui/ON_LOAD_EVENT', payload: eventId })
+    }
+  }
+
+  // when Event route, set route eventId as app event context
   if (isRoute('HOME_EVENT')(state)) {
-    // set route eventId as app event context
     const eventId = getRouterParam('eventId')(state)
     if (eventId !== store.ui.app.get().currentEventId) {
       localStorage.setItem('currentEventId', eventId)
       store.ui.app.update({ currentEventId: eventId })
     }
   }
+
+  // when Invite speaker route, load invite talk and user
   if (isRoute('INVITE_SPEAKER')(state)) {
     const talkId = getRouterParam('talkId')(state)
     const uid = getRouterParam('uid')(state)
