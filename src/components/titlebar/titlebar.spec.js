1ca23e4fce6945a11fd46ca19139e8198c117908
Benjamin Petetot <bpetetot@gmail.com>
2018-02-26 22:17:43 +0100

    titlebar component

---
+++ src/components/titlebar/titlebar.spec.js
@@ -0,0 +1,13 @@
+/* eslint-env jest */
+import snap from 'tests/snapshot'
+import Titlebar from './index.js'
+
+jest.mock('../iconLabel', () => 'IconLabel')
+
+const snapshot = props => snap(Titlebar)({ ...props })
+
+describe('components/titlebar', () => {
+  it('should render', snapshot({
+    icon: 'i', title: 't', children: 'c', className: 'cn',
+  }))
+})
