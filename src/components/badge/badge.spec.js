b97abacdb3d5c4a421a6e885bae958279713872e
Benjamin Petetot <bpetetot@gmail.com>
2018-02-26 21:06:34 +0100

    badge component tests

---
+++ src/components/badge/badge.spec.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+import snap from 'tests/snapshot'
+import Badge from './index.js'
+
+const snapshot = props => snap(Badge)({ ...props })
+
+describe('components/badge', () => {
+  it('should not render when no children', snapshot({}))
+  it('should render with children', snapshot({ children: 'Benjamin', className: 'test' }))
+})
