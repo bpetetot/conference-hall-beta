7bd4db9a095f095c344b711b40e5c768dee01ba5
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-02-26 19:09:12 +0100

    :sparkles: Add newly created organization in user organizations property

--- src/store/listeners/reactions/organizations.js
+++ src/store/listeners/reactions/organizations.js
@@ -20,6 +20,9 @@ export const createOrganization = reaction(async (action, store, { form }) => {
   store.data.organizations.add(organization)
   store.data.users.addOrUpdate(user)
 
+  // Add organization id in owner organizations property
+  store.dispatch({ type: '@@ui/ADD_ORGANIZATION_TO_USER', payload: { uid, organizationId: ref.id } })
+
   // FIXME: Go to newly created organization page
   // go to organization page
   store.dispatch(push('/organizer/organizations'))
