adba569d596528f91358dc5c90747c145f2460e2
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-03-05 23:10:51 +0100

    :recycle: Use generic components for co-speaker feature

--- src/screens/components/talk/speakers/addSpeaker/addSpeakerButton.container.js
+++ src/screens/components/talk/speakers/addSpeaker/addSpeakerButton.container.js
@@ -1,16 +1,19 @@
 import { inject } from '@k-ramel/react'
-
 import { getRouterParam } from 'store/reducers/router'
-import SpeakerResults from './speakerResults'
+import AddSpeakerButton from './addSpeakerButton'
 
 const mapStore = (store) => {
+  const { uid: authId } = store.auth.get()
   const talkId = getRouterParam('talkId')(store.getState())
+  const url = window.location.href.split('/')
+
   return {
-    onSelectSpeaker: (uid) => {
+    inviteLink: `${url[0]}//${url[2]}/speaker/invite/talk/${talkId}/${authId}`,
+    onSelectUser: (uid) => {
       store.dispatch({ type: '@@ui/ADD_SPEAKER_TO_TALK', payload: { uid, talkId } })
       store.ui.modal.set({ openedModal: undefined })
     },
   }
 }
 
-export default inject(mapStore)(SpeakerResults)
+export default inject(mapStore)(AddSpeakerButton)
