2484f516aefb162416e35ac0e593a6c39489a8e3
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-03-05 15:26:36 +0100

    :recycle: Set up a batch for creating new organization (#181)
    * :recycle: Set up a batch for creating new organization
    
    * :rotating_light: Fix lint warnings
    
    * :bug: Fix some bugs
    
    * :ok_hand: Take care of @bpetetot review

--- src/firebase/organizations.js
+++ src/firebase/organizations.js
@@ -1,3 +1,33 @@
-import crud from './crud'
+import firebase from 'firebase/app'
+import { set } from 'immutadot'
 
-export default crud('organizations', 'id')
+import crud from './crud'
+import userCrud from './user'
+
+const organizationCrud = crud('organizations', 'id')
+
+export const createOrganization = async (organization, uid) => {
+  const db = firebase.firestore()
+  const batch = db.batch()
+
+  const { id: organizationId } = await organizationCrud.create(organization)
+
+  // FIXME: https://github.com/bpetetot/conference-hall/issues/167
+  const organizationRef = await organizationCrud.read(organizationId)
+  const updatedOrganization = {
+    id: organizationRef.id,
+    owner: uid,
+    ...organizationRef.data(),
+  }
+  organizationCrud.update(updatedOrganization)
+
+  const userRef = await userCrud.read(uid)
+  const updatedUser = set(userRef.data(), `organizations.${organizationId}`, true)
+  await userCrud.update(updatedUser)
+
+  await batch.commit()
+
+  return [updatedOrganization, updatedUser]
+}
+
+export default organizationCrud
--- src/store/listeners/reactions/organizations.js
+++ src/store/listeners/reactions/organizations.js
@@ -1,27 +1,24 @@
 import { reaction } from 'k-ramel'
 import { push } from 'redux-little-router'
 
-import organizationCrud from 'firebase/organizations'
+import { createOrganization as firebaseCreateOrganization } from 'firebase/organizations'
 
 // FIXME: change it when other operations will be implemented
 // eslint-disable-next-line import/prefer-default-export
 export const createOrganization = reaction(async (action, store, { form }) => {
   const createForm = form('organization-create')
-  const organization = createForm.getFormValues()
+  const organizationValues = createForm.getFormValues()
   // get user id
   const { uid } = store.auth.get()
   // create organization into database
-  const { id } = await createForm.asyncSubmit(
-    organizationCrud.create,
-    { ...organization, owner: uid },
+  const [organization, user] = await createForm.asyncSubmit(
+    firebaseCreateOrganization,
+    organizationValues,
+    uid,
   )
 
-  // FIXME: https://github.com/bpetetot/conference-hall/issues/167
-  const ref = await organizationCrud.read(id)
-  organizationCrud.update({ id: ref.id, ...ref.data() })
-
-  // Add organization id in owner organizations property
-  store.dispatch({ type: '@@ui/ADD_ORGANIZATION_TO_USER', payload: { uid, organizationId: ref.id } })
+  store.data.organizations.add(organization)
+  store.data.users.addOrUpdate(user)
 
   // FIXME: Go to newly created organization page
   // go to organization page
