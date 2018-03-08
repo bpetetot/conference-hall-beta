f006c4a13948dbc165a121e384dcb572cfcf1380
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2018-03-08 11:53:24 +0100

    :sparkles: Talk submission from My talks (#186)
    * :construction: Add submit button in submissions card of talk
    
    * :lipstick: Submit talk button style
    
    * :construction: Add event selection page
    
    * :sparkles: Events selection page
    
    * :ok_hand: Review @bpetetot
    
    * :ok_hand: Review @bpetetot

--- src/store/reducers/ui/speaker/index.js
+++ src/store/reducers/ui/speaker/index.js
@@ -1,8 +1,10 @@
+import myEvents from './myEvents'
 import myTalks from './myTalks'
 import submission from './submission'
 import speakerAddModal from './speakerAddModal'
 
 export default {
+  myEvents,
   myTalks,
   submission,
   speakerAddModal,
