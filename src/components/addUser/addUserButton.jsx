b8ac7f5b7b8e62972c411cdabb3d4eeada729aff
Benjamin Petetot <bpetetot@gmail.com>
2018-04-05 19:16:12 +0200

    :bug: set correct proptypes for addUserButton

--- src/components/addUser/addUserButton.jsx
+++ src/components/addUser/addUserButton.jsx
@@ -33,7 +33,7 @@ AddUserButton.propTypes = {
   label: PropTypes.string,
   modalOptions: PropTypes.shape({
     title: PropTypes.string,
-    message: PropTypes.string.isRequired,
+    message: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
     resultsMessage: PropTypes.string.isRequired,
     inviteLink: PropTypes.string.isRequired,
     onSelectUser: PropTypes.func.isRequired,
