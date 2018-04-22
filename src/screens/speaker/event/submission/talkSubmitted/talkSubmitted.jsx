79b1789f428242f4b3ca60edf99e5a7d6076f9be
Benjamin Petetot <bpetetot@gmail.com>
2018-04-22 20:55:33 +0200

    :sparkles: add link to survey speaker after submitting

--- src/screens/speaker/event/submission/talkSubmitted/talkSubmitted.jsx
+++ src/screens/speaker/event/submission/talkSubmitted/talkSubmitted.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'redux-little-router'
 
 import SubmitTalksLink from 'screens/components/submitTalksLink'
 import './talkSubmitted.css'
 
 const TalkSubmitted = ({ eventId, eventName }) => (
   <div className="icon-submitted">
-    <i className="fa fa-paper-plane-o" />
     <h2>Congrats ! Talk successfully submitted to {eventName}</h2>
+    <h1><Link href={`/speaker/event/${eventId}/survey`}>Please fill the speaker survey</Link></h1>
+    <i className="fa fa-paper-plane-o" />
+    <br />
     <SubmitTalksLink eventId={eventId} label="Want to submit or update an other talk ?" />
   </div>
 )
