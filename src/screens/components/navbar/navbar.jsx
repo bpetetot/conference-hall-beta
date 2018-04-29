370f2fdaee6fd50d86c9a27076bb8b941662e0ea
Benjamin Petetot <bpetetot@gmail.com>
2018-04-29 12:58:14 +0200

    :fire: remove bug report link

--- src/screens/components/navbar/navbar.jsx
+++ src/screens/components/navbar/navbar.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { GITHUB_ISSUES } from 'helpers/github'
 import Navbar from 'components/navbar'
-import IconLink from 'components/iconLink'
 import AvatarDropdown from './avatarDropdown'
 
 import './navbar.css'
 
 const SpeakerNavbar = ({ className }) => (
   <Navbar className={className}>
-    <IconLink
-      icon="fa fa-github"
-      label="report a bug"
-      className="navbar-link"
-      href={GITHUB_ISSUES}
-    />
     <AvatarDropdown />
   </Navbar>
 )
