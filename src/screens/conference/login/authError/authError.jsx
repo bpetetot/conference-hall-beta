ea9653346a69bba2e5e6b38e1bc2d0f98ded4e63
Benjamin Petetot <bpetetot@gmail.com>
2018-05-31 18:13:59 +0200

    :memo: improve error message

--- src/screens/conference/login/authError/authError.jsx
+++ src/screens/conference/login/authError/authError.jsx
@@ -7,8 +7,56 @@ import './authError.css'
 
 const AuthErrorModal = ({ error, clearAuthError }) => (
   <DumbModal opened={!!error.message} onClose={clearAuthError} className="auth-error">
-    <div>{error.code}</div>
-    <div>{error.message}</div>
+    <h2>Sorry, an error occured during login</h2>
+    {error.code === 'auth/web-storage-unsupported' ? (
+      <div>
+        <p>
+          Conference Hall app needs cookies to be enabled in order to works with the Firebase Auth
+          system. But your browser blocks 3rd party cookies. You can enable cookies using one of
+          this process following your browser.
+        </p>
+        <div>
+          <p>
+            <strong>Google Chrome</strong>
+            <ol>
+              <li>At the top right, click the More button, then Settings.</li>
+              <li>At the bottom, click Advanced.</li>
+              <li>Under &apos;Privacy and security&apos;, click Content settings.</li>
+              <li>Click Cookies.</li>
+              <li>Turn off Block third-party cookies.</li>
+            </ol>
+          </p>
+          <p>
+            <strong>Apple Safari</strong>
+            <ol>
+              <li>Go to Safari › Preferences.</li>
+              <li>Click Privacy.</li>
+              <li>For Cookies and website data, select Always allow.</li>
+            </ol>
+          </p>
+          <p>
+            <strong>Mozilla Firefox</strong>
+            <ol>
+              <li>At the top right, click the Menu button, then Options.</li>
+              <li>Click Privacy &amp; Security.</li>
+              <li>
+                Under History, set the drop-down menu next to Firefox will to Remember History.
+              </li>
+            </ol>
+          </p>
+          <p>
+            <strong>Microsoft Edge</strong>
+            <ol>
+              <li>At the top right, click the Menu button, then Settings.</li>
+              <li>Click View Advanced Settings.</li>
+              <li>Set Cookies to Don&apos;t block cookies.</li>
+            </ol>
+          </p>
+        </div>
+      </div>
+    ) : (
+      <div>{error.message}</div>
+    )}
   </DumbModal>
 )
 
