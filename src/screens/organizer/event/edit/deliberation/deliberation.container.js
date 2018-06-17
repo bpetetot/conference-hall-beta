e1306d2efcee54a358c5214a851bb3fa3aa89603
Benjamin Petetot <bpetetot@gmail.com>
2018-06-17 12:31:57 +0200

    :recycle: refactor event forms (#254)

--- src/screens/organizer/event/edit/deliberation/deliberation.container.js
+++ src/screens/organizer/event/edit/deliberation/deliberation.container.js
@@ -1,24 +1,24 @@
 import { compose } from 'redux'
 import { inject } from '@k-ramel/react'
-import { reduxForm } from 'redux-form'
 import forRoute from 'hoc-little-router'
 
 import DeliberationForm from './deliberation'
 
-const FORM_NAME = 'deliberation-edit'
-
-const mapStore = (store, { eventId }, { form }) => {
-  const event = store.data.events.get(eventId)
-  const { deliberationActive } = form(FORM_NAME).getFormValues() || {}
+const mapStore = (store, { eventId }) => {
+  const { deliberationActive } = store.data.events.get(eventId) || {}
   return {
     deliberationActive,
-    initialValues: event,
-    onSubmit: () => store.dispatch('@@ui/ON_UPDATE_EVENT_DELIBERATION'),
+    onActiveDeliberation: e =>
+      store.dispatch({
+        type: '@@ui/ON_TOGGLE_EVENT_DELIBERATION',
+        payload: {
+          event: {
+            id: eventId,
+            deliberationActive: e.target.checked,
+          },
+        },
+      }),
   }
 }
 
-export default compose(
-  forRoute.absolute('EDIT_EVENT_DELIBERATION'),
-  inject(mapStore),
-  reduxForm({ form: FORM_NAME }),
-)(DeliberationForm)
+export default compose(forRoute.absolute('EDIT_EVENT_DELIBERATION'), inject(mapStore))(DeliberationForm)
