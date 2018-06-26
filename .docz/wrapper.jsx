c267eded9bd6fa0e8faa87f2b52aeabebd0c3bad
Benjamin Petetot <bpetetot@gmail.com>
2018-06-27 01:20:40 +0200

    refactor for docz

--- .docz/wrapper.jsx
+++ .docz/wrapper.jsx
@@ -1,12 +1,17 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
+import React, { Component } from 'react';
 
 import 'font-awesome/css/font-awesome.min.css'
 import 'src/styles/themes/default.css'
 import 'src/styles/components/button.css'
 
-export default ({ children }) => (
-  <div className="default-theme">
-    {children}
-  </div>
-)
+class Wrapper extends Component {
+  render() {
+    return (
+      <div className="default-theme">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Wrapper;
