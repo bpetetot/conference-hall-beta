8b2ec4c1c9f620b87a01575353b1dd117db16d80
Benjamin Petetot <bpetetot@gmail.com>
2018-06-29 00:30:23 +0200

    :sparkles: button component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -5,9 +5,36 @@ import cn from 'classnames'
 import './button.css'
 
 const Button = ({
-  className, icon, children, ...rest
+  className,
+  type,
+  size,
+  accent,
+  error,
+  block,
+  icon,
+  loading,
+  simple,
+  children,
+  ...rest
 }) => {
-  const classes = cn('cc-button', { 'cc-button-icon': icon }, className)
+  const classes = cn(
+    'cc-button',
+    {
+      'cc-button-icon': icon,
+      'cc-button-block': block,
+      'cc-button-small': size === 'small',
+      'cc-button-large': size === 'large',
+      'cc-button-accent': accent,
+      'cc-button-error': error,
+      'cc-button-primary': type === 'primary',
+      'cc-button-secondary': type === 'secondary',
+      'cc-button-tertiary': type === 'tertiary',
+      'cc-button-loading': loading,
+      'cc-button-simple': simple,
+    },
+    className,
+  )
+
   return (
     <button className={classes} {...rest}>
       {children}
@@ -18,11 +45,25 @@ const Button = ({
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   icon: PropTypes.bool,
+  block: PropTypes.bool,
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
+  size: PropTypes.oneOf(['normal', 'small', 'large']),
+  accent: PropTypes.bool,
+  error: PropTypes.bool,
+  loading: PropTypes.bool,
+  simple: PropTypes.bool,
   className: PropTypes.string,
 }
 
 Button.defaultProps = {
   icon: false,
+  block: false,
+  type: 'primary',
+  size: 'normal',
+  accent: false,
+  error: false,
+  loading: false,
+  simple: false,
   className: undefined,
 }
 
