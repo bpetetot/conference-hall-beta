1aa4d4a53f5ef71052702d7ea2062b1272f8859e
Benjamin Petetot <bpetetot.external@lafourchette.com>
2018-07-24 14:10:39 +0200

    generate color by default or set color from props

--- src/components/avatar/avatar.jsx
+++ src/components/avatar/avatar.jsx
@@ -2,18 +2,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
+import randomColor from 'randomcolor'
 
 import './avatar.css'
 
-const Avatar = ({ name, src, className }) => {
+const generateColor = (seed) => {
+  if (!seed) return {}
+  return {
+    background: randomColor({
+      seed,
+      luminosity: 'bright',
+      hsla: 'rgb',
+    }),
+  }
+}
+
+const Avatar = ({
+  name, src, color, className, style,
+}) => {
   if (!name && !src) return null
-  const classes = cn(
-    'avatar',
-    { 'avatar-initials': !src, 'avatar-image': src },
-    className,
-  )
+
+  const colors = color ? { background: color } : generateColor(name)
+  const classes = cn('avatar', { 'avatar-initials': !src, 'avatar-image': src }, className)
+
   return (
-    <div className={classes}>
+    <div className={classes} style={{ ...colors, ...style }}>
       {src && <img src={src} alt="avatar" />}
       {!src && name && <span>{name.charAt(0)}</span>}
     </div>
@@ -23,13 +36,17 @@ const Avatar = ({ name, src, className }) => {
 Avatar.propTypes = {
   name: PropTypes.string,
   src: PropTypes.string,
+  color: PropTypes.string,
   className: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.string),
 }
 
 Avatar.defaultProps = {
   name: undefined,
   src: undefined,
+  color: undefined,
   className: undefined,
+  style: undefined,
 }
 
 export default Avatar
