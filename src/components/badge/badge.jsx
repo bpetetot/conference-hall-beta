138762b0ea173881b51df1d4153879a823c7fb23
Benjamin Petetot <bpetetot@gmail.com>
2018-07-24 21:05:21 +0200

    add badge status

--- src/components/badge/badge.jsx
+++ src/components/badge/badge.jsx
@@ -4,15 +4,38 @@ import cn from 'classnames'
 
 import './badge.css'
 
-const Badge = ({ children, pill, className, style }) => {
+const Badge = ({
+  children,
+  pill,
+  primary,
+  secondary,
+  success,
+  warning,
+  error,
+  info,
+  dark,
+  light,
+  className,
+  style
+}) => {
   if (!children) return null
 
   const classes = cn(
     'cc-badge',
-    'cc-badge-default',
-    { 'cc-badge-pill': pill },
+    {
+      'cc-badge-pill': pill,
+      'cc-badge-primary': primary,
+      'cc-badge-secondary': secondary,
+      'cc-badge-success': success,
+      'cc-badge-warning': warning,
+      'cc-badge-error': error,
+      'cc-badge-info': info,
+      'cc-badge-dark': dark,
+      'cc-badge-light': light,
+    },
     className
   )
+
   return (
     <span className={classes} style={style}>
       {children
@@ -23,6 +46,14 @@ const Badge = ({ children, pill, className, style }) => {
 Badge.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]),
   pill: PropTypes.bool,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  error: PropTypes.bool,
+  info: PropTypes.bool,
+  dark: PropTypes.bool,
+  light: PropTypes.bool,
   className: PropTypes.string,
   style: PropTypes.object,
 }
@@ -30,6 +61,14 @@ Badge.propTypes = {
 Badge.defaultProps = {
   children: undefined,
   pill: false,
+  primary: true,
+  secondary: false,
+  success: false,
+  warning: false,
+  error: false,
+  info: false,
+  dark: false,
+  light: false,
   className: undefined,
   style: undefined,
 }
