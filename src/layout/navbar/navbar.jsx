3380285bb7994458108d7851971ad446237419ec
Benjamin Petetot <bpetetot@gmail.com>
2018-11-03 15:32:01 +0100

    :sparkles: report a bug link

--- src/layout/navbar/navbar.jsx
+++ src/layout/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
+import IconLink from 'components/iconLink'
 
 import AvatarDropdown from './avatarDropdown'
 
@@ -8,6 +9,12 @@ import './navbar.css'
 
 const Navbar = ({ className }) => (
   <nav className={cn('navbar', className)}>
+    <IconLink
+      icon="fa fa-github"
+      label="report a bug"
+      className="navbar-link"
+      href="https://github.com/bpetetot/conference-hall/issues/new"
+    />
     <AvatarDropdown />
   </nav>
 )
