c8dcb447db1322bd7aa3541fb3fd27da16eaa3c7
Benjamin Petetot <bpetetot@gmail.com>
2018-11-04 14:38:34 +0100

    :sparkles: use correct timezone to display cfp state

--- src/firebase/functionCalls.js
+++ src/firebase/functionCalls.js
@@ -1,11 +1,21 @@
 import firebase from 'firebase/app'
+import { DateTime } from 'luxon'
 
 const functions = {}
 
+const buildFunctionWithTimezone = (functionName) => {
+  const cloudFunction = firebase.functions().httpsCallable(functionName)
+  const timezone = DateTime.local.zoneName
+
+  return ({ userTimezone, ...rest }) => cloudFunction({
+    ...rest,
+    userTimezone: userTimezone || timezone,
+  })
+}
+
 export const initFunctionCalls = () => {
-  functions.getCfpState = firebase.functions().httpsCallable('getCfpState')
-  functions.submitTalk = firebase.functions().httpsCallable('submitTalk')
-  functions.unsubmitTalk = firebase.functions().httpsCallable('unsubmitTalk')
+  functions.submitTalk = buildFunctionWithTimezone('submitTalk')
+  functions.unsubmitTalk = buildFunctionWithTimezone('unsubmitTalk')
 }
 
 export default functions
