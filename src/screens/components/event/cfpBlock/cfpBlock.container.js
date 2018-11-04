2b806c7040f119d25988f5e11b29edf80782759f
Benjamin Petetot <bpetetot@gmail.com>
2018-11-04 13:37:48 +0100

    :fire: remove useless code

--- src/screens/components/event/cfpBlock/cfpBlock.container.js
+++ src/screens/components/event/cfpBlock/cfpBlock.container.js
@@ -1,14 +1,10 @@
 import { inject } from '@k-ramel/react'
-import firebase from 'firebase/app'
 
 import { getCfpState } from 'store/reducers/data/events.selector'
 import CfpBlock from './cfpBlock'
 
 const mapStore = (store, { eventId }) => {
   const { type, cfpDates, deliberationDate } = store.data.events.get(eventId)
-
-  const getCfpStateFunction = firebase.functions().httpsCallable('getCfpState')
-  getCfpStateFunction({ eventId }).then(console.log)
 
   return {
     cfpState: getCfpState(eventId)(store),
