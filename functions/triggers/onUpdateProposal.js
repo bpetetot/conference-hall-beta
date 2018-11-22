12b5bd3b445a00a9fb0f82878e367490ba5acb0f
Benjamin Petetot <bpetetot@gmail.com>
2018-11-22 21:49:33 +0100

    :zap: optimize computing in deliberation email trigger

--- functions/triggers/onUpdateProposal.js
+++ functions/triggers/onUpdateProposal.js
@@ -12,27 +12,35 @@ const talkRejected = require('../email/templates/talkRejected')
 module.exports = functions.firestore
   .document('events/{eventId}/proposals/{proposalId}')
   .onUpdate(async (snap, context) => {
+    const previousTalk = snap.before.data()
     const talk = snap.after.data()
-    console.log(`:::update::talk ${JSON.stringify(talk)}`)
+
+    // if proposal state didn't changed or email already sent, dont need to go further
+    if (previousTalk.state === talk.state || talk.emailSent) {
+      return null
+    }
 
     const { eventId } = context.params
     const event = await getEvent(eventId)
 
+    // if deliberation email disabled, dont need to go further
     if (!event.sendDeliberationEmails) {
       return null
     }
 
-    const uids = Object.keys(talk.speakers)
+    // check mailgun configuration
     const { app, mailgun } = functions.config()
     if (!app) return Promise.reject(new Error('You must provide the app.url variable'))
+
+    const uids = Object.keys(talk.speakers)
     const submissionUpdate = {
       submissions: {
         [eventId]: { ...talk },
       },
     }
 
+    // send email to accepted proposal
     if (talk.state === 'accepted' && !talk.emailSent) {
-      // set emailSent to make sure we send accepted email only once.
       talk.emailSent = talk.updateTimestamp
       console.log(`:::update::accepted ${JSON.stringify(submissionUpdate)}`)
       return Promise.all([
@@ -46,8 +54,8 @@ module.exports = functions.firestore
       }))
     }
 
+    // send email to rejected proposal
     if (talk.state === 'rejected' && !talk.emailSent) {
-      // set emailSent to make sure we send declined email only once.
       talk.emailSent = talk.updateTimestamp
       console.log(`:::update::rejected ${JSON.stringify(submissionUpdate)}`)
       return Promise.all([
