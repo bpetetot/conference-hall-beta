bb403d9568c9d9986b390e00421455cbf4c0a767
Benjamin Petetot <bpetetot@gmail.com>
2018-11-22 21:35:15 +0100

    :sparkles: dont disable deliberation if auto send email is disabled

--- src/screens/organizer/components/talkSelection/talkSelection.jsx
+++ src/screens/organizer/components/talkSelection/talkSelection.jsx
@@ -1,64 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TalkSelection = ({ onChange, defaultValue }) => {
-  const talkDeliberation = defaultValue
-  return (
-    <div>
-      {talkDeliberation === 'accepted' || talkDeliberation === 'rejected' ? (
-        <select
-          id="ratings"
-          onChange={onChange}
-          onClick={e => e.stopPropagation()}
-          defaultValue={defaultValue}
-          disabled
-        >
-          <option key="submitted" value="submitted">
-            Deliberate...
-          </option>
-          <option key="accepted" value="accepted">
-            Accepted
-          </option>
-          <option key="backup" value="backup">
-            Backup
-          </option>
-          <option key="rejected" value="rejected">
-            Rejected
-          </option>
-        </select>
-      ) : (
-        <select
-          id="ratings"
-          onChange={onChange}
-          onClick={e => e.stopPropagation()}
-          defaultValue={defaultValue}
-        >
-          <option key="submitted" value="submitted">
-            Deliberate...
-          </option>
-          <option key="accepted" value="accepted">
-            Accepted
-          </option>
-          <option key="backup" value="backup">
-            Backup
-          </option>
-          <option key="rejected" value="rejected">
-            Rejected
-          </option>
-        </select>
-      )}
-
-    </div>
-  )
-}
+const TalkSelection = ({ onChange, defaultValue, disabled }) => (
+  <select
+    id="ratings"
+    onChange={onChange}
+    onClick={e => e.stopPropagation()}
+    defaultValue={defaultValue}
+    disabled={disabled}
+  >
+    <option key="submitted" value="submitted">
+      Deliberate...
+    </option>
+    <option key="accepted" value="accepted">
+      Accepted
+    </option>
+    <option key="backup" value="backup">
+      Backup
+    </option>
+    <option key="rejected" value="rejected">
+      Rejected
+    </option>
+  </select>
+)
 
 TalkSelection.propTypes = {
   onChange: PropTypes.func.isRequired,
   defaultValue: PropTypes.string,
+  disabled: PropTypes.bool,
 }
 
 TalkSelection.defaultProps = {
   defaultValue: undefined,
+  disabled: false,
 }
 
 export default TalkSelection
