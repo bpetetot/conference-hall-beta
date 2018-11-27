5cd9e28a06386a2371554db6c8bd485734ceeb15
Benjamin Petetot <bpetetot@gmail.com>
2018-11-27 21:25:54 +0100

    ♻️use of react-datepicker (#378)

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom'
 import 'normalize.css'
 import 'font-awesome/css/font-awesome.min.css'
 import 'regenerator-runtime/runtime'
-import 'react-dates/initialize'
-import 'react-dates/lib/css/_datepicker.css'
 
 import App from './app'
 
