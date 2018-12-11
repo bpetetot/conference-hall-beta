1410ec52689df8cdffcbfaebfff6c06b8f8a9366
Benjamin Petetot <bpetetot.external@lafourchette.com>
2018-12-11 17:05:13 +0100

    :recycle: simplify api authentication checker

--- functions/api/middlewares/auth.js
+++ functions/api/middlewares/auth.js
@@ -11,42 +11,19 @@ const admin = require('firebase-admin')
  * source: https://github.com/firebase/functions-samples/blob/master/authorized-https-endpoint/functions/index.js
  */
 module.exports = (req, res, next) => {
-  console.log('Check if request is authorized with Firebase ID token')
+  const { authorization } = req.headers
 
-  if (
-    (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))
-    && !(req.cookies && req.cookies.__session)
-  ) {
-    console.error(
-      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
-      'Make sure you authorize your request by providing the following HTTP header:',
-      'Authorization: Bearer <Firebase ID Token>',
-      'or by passing a "__session" cookie.',
-    )
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     res.status(403).send('Unauthorized')
     return
   }
 
-  let idToken
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-    console.log('Found "Authorization" header')
-    // Read the ID Token from the Authorization header.
-    idToken = req.headers.authorization.split('Bearer ')[1]
-  } else if (req.cookies) {
-    console.log('Found "__session" cookie')
-    // Read the ID Token from cookie.
-    idToken = req.cookies.__session
-  } else {
-    // No cookie
-    res.status(403).send('Unauthorized')
-    return
-  }
+  const idToken = authorization.split('Bearer ')[1]
 
   admin
     .auth()
     .verifyIdToken(idToken)
     .then((decodedIdToken) => {
-      console.log('ID Token correctly decoded', decodedIdToken)
       req.user = decodedIdToken
       return next()
     })
