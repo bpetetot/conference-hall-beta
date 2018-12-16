4bdac7230e14b033a7cddd13e014bad13a1b3e7f
Benjamin Petetot <bpetetot@gmail.com>
2018-12-16 18:20:34 +0100

    :recycle: remove unused code

--- src/screens/conference/login/login.container.js
+++ src/screens/conference/login/login.container.js
@@ -7,7 +7,6 @@ import Login from './login'
 const mapStore = store => ({
   ...store.auth.get(),
   signin: provider => store.dispatch({ type: '@@ui/SIGN_IN', payload: provider }),
-  clearAuthError: () => store.auth.update({ error: {} }),
 })
 
 export default compose(
