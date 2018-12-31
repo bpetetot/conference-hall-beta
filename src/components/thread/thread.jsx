cccaac8272c32dbf8c8616113fec0518e0f41a56
Benjamin Petetot <bpetetot@gmail.com>
2018-12-31 14:01:22 +0100

    :sparkles: add input in thread

--- src/components/thread/thread.jsx
+++ src/components/thread/thread.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
 import Message from './message'
 import styles from './thread.module.css'
 
-const Threads = ({ messages, className }) => (
-  <div className={className}>
-    {messages.map(({ id, ...message }) => (
-      <Message key={id} {...message} className={styles.message} />
-    ))}
-  </div>
-)
+class Thread extends Component {
+  thread = React.createRef();
 
-Threads.propTypes = {
+  componentDidMount() {
+    if (this.thread) {
+      const { current } = this.thread
+      current.scrollTop = current.scrollHeight
+    }
+  }
+
+  render() {
+    const { messages, className } = this.props
+    return (
+      <div className={cn(styles.thread, className)}>
+        <div ref={this.thread} className={styles.messages}>
+          {messages.map(({ id, ...message }) => (
+            <Message key={id} {...message} className={styles.message} />
+          ))}
+        </div>
+        <div className={styles.input}>
+          <input type="text" name="message" placeholder="Send a message" />
+        </div>
+      </div>
+    )
+  }
+}
+
+Thread.propTypes = {
   messages: PropTypes.arrayOf(PropTypes.shape(Message.propTypes)),
   className: PropTypes.string,
 }
 
-Threads.defaultProps = {
+Thread.defaultProps = {
   messages: [],
   className: undefined,
 }
 
-export default Threads
+export default Thread
