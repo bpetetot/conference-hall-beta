a176c16a001998823af497d3a0f5f7ee432d99a4
Benjamin Petetot <bpetetot@gmail.com>
2019-01-10 22:50:25 +0100

    :recycle: move router files

--- src/store/drivers/index.js
+++
@@ -1,5 +0,0 @@
-import router from './router'
-
-export default {
-  router,
-}
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { createStore } from 'k-ramel'
+import { router } from '@k-redux-router/react-k-ramel'
 
 import reducers from './reducers'
 import listeners from './listeners'
-import drivers from './drivers'
+import routes from './routes'
 
 /**
  * Store and dispatch actions are managed with k-ramel
@@ -10,7 +11,13 @@ import drivers from './drivers'
  */
 const store = createStore(reducers, {
   listeners,
-  drivers,
+  drivers: {
+    router: router({
+      routes,
+      state: 'ui.router',
+      getState: state => state.ui.router,
+    }),
+  },
   devtools: true,
 })
 
