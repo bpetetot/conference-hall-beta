3f35b487e3976826f36b7ca0aed3480bfa081538
Benjamin Petetot <bpetetot.external@lafourchette.com>
2019-02-01 11:35:50 +0100

    :bug: fix ratings when get a slow connexion

--- src/store/reducers/data/ratings.selectors.js
+++ src/store/reducers/data/ratings.selectors.js
@@ -1,14 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import isEmpty from 'lodash/isEmpty'
 
-export const getRatingsAverage = (store) => {
-  const ratings = store.data.ratings.getAsArray().filter(r => r.feeling !== 'noopinion')
-  if (isEmpty(ratings)) return null
-  return ratings.map(r => r.rating).reduce((p, c) => p + c, 0) / ratings.length
+export const getRatingsAverage = (ratings) => {
+  const usersRatings = ratings.filter(r => r.feeling !== 'noopinion')
+  if (isEmpty(usersRatings)) return null
+  return usersRatings.map(r => r.rating).reduce((p, c) => p + c, 0) / usersRatings.length
 }
 
-export const getFeelingsCount = feeling => (store) => {
-  const ratings = store.data.ratings.getAsArray()
+export const getFeelingsCount = feeling => (ratings) => {
   if (isEmpty(ratings)) return null
   return ratings.filter(r => r.feeling === feeling).length
 }
