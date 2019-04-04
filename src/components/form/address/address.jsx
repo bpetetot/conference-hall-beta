89fef4989b8b3137e840682e5ebf9cf0f05ff04c
Benjamin Petetot <bpetetot@gmail.com>
2019-04-04 23:28:49 +0200

    :sparkles: extract lat-lng and details of address

--- src/components/form/address/address.jsx
+++ src/components/form/address/address.jsx
@@ -1,31 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import cn from 'classnames'
-import PlacesAutocomplete from 'react-places-autocomplete'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import flow from 'lodash/fp/flow'
+import filter from 'lodash/fp/filter'
+import first from 'lodash/fp/first'
+import pick from 'lodash/fp/pick'
 
 import './address.css'
 
-const AddressInput = props => (
-  <PlacesAutocomplete {...props}>
-    {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-      <div className="cc-address-input-wrapper">
-        <input {...getInputProps({ autoComplete: 'nope' })} />
-
-        <div className="cc-address-input-suggestions">
-          {suggestions.map((suggestion) => {
-            const className = cn('cc-address-input-suggestion-item', {
-              'cc-address-input-suggestion-item-active': suggestion.active,
-            })
-
-            return (
-              <div {...getSuggestionItemProps(suggestion, { className })}>
-                <span>{suggestion.description}</span>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    )}
-  </PlacesAutocomplete>
+const getAddressComponent = name => flow(
+  filter(component => component.types.includes(name)),
+  first,
+  pick(['short_name', 'long_name']),
 )
+
+const AddressInput = (props) => {
+  const [address, setAddress] = useState(props.value && props.value.formattedAddress)
+
+  const handleSelect = async (selectedAddress) => {
+    const results = await geocodeByAddress(selectedAddress)
+    const result = first(results)
+    if (!result) return
+
+    const latLng = await getLatLng(result)
+    const country = getAddressComponent('country')(result.address_components)
+    const locality = getAddressComponent('locality')(result.address_components)
+
+    setAddress(selectedAddress)
+    props.onChange({
+      formattedAddress: selectedAddress,
+      locality,
+      country,
+      latLng,
+    })
+  }
+
+  return (
+    <PlacesAutocomplete {...props} value={address} onChange={setAddress} onSelect={handleSelect}>
+      {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+        <div className="cc-address-input-wrapper">
+          <input {...getInputProps({ autoComplete: 'nope' })} />
+
+          <div className="cc-address-input-suggestions">
+            {suggestions.map((suggestion) => {
+              const className = cn('cc-address-input-suggestion-item', {
+                'cc-address-input-suggestion-item-active': suggestion.active,
+              })
+
+              return (
+                <div {...getSuggestionItemProps(suggestion, { className })}>
+                  <span>{suggestion.description}</span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
+  )
+}
+
+AddressInput.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  onChange: PropTypes.func.isRequired,
+}
+
+AddressInput.defaultProps = {
+  value: undefined,
+}
 
 export default AddressInput
