3f99ba9cfbe4b3c87ff5586b567e5d5efaf9a603
Corinne Krych <corinnekrych@gmail.com>
2019-04-06 13:06:02 +0200

    test inline

---
+++ src/components/inline/inline.spec.jsx
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+import React from 'react'
+import { shallow } from 'enzyme'
+import Inline from './index'
+
+describe('components/inline', () => {
+  it('should render', () => {
+    expect(
+      shallow(
+        <Inline className="proposal-subtitle" classNameItem="proposal-subtitle-item">
+          {'speakerName'}
+          {'formatLabel'}
+          {'categoryLabel'}
+        </Inline>,
+      ),
+    ).toMatchSnapshot()
+  })
+})
