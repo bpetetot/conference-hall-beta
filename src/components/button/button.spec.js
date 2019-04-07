0422231bd8b35e7bce6be4b4e7f2e67409af6fca
Corinne Krych <corinnekrych@gmail.com>
2019-04-07 19:57:19 +0200

    👻 snap on form component

--- src/components/button/button.spec.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-import snap from 'tests/snapshot'
-import Button from './index.js'
-
-const snapshot = props => snap(Button)({ ...props })
-
-describe('components/button', () => {
-  it('should render with children', snapshot({ children: 'Benjamin', className: 'test', secondary: true }))
-})
