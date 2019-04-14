1324b7d26b6f180525c1a699ed4fc378a5a4b3bf
Yvonnick FRIN <yvonnickfrin@macbook-pro-de-yvonnick-2.home>
2019-04-14 15:36:20 +0200

    :sparkles: Add agenda page in event section

--- src/store/routes.js
+++ src/store/routes.js
@@ -40,6 +40,9 @@ export default {
           '/deliberation': { code: 'organizer-event-edit-deliberation' },
           '/integrations': { code: 'organizer-event-edit-integrations' },
         },
+        '/agenda': {
+          code: 'organizer-event-agenda',
+        },
         '/proposals': {
           code: 'organizer-event-proposals',
           sortOrders: ['newest', 'oldest', 'highestRating', 'lowestRating'],
