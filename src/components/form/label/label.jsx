34c110d2d6507d74e721d5ecf75e13b2755f5d10
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-05-17 12:25:34 +0200

    :sparkles: Allow to add tooltip on Labels

--- src/components/form/label/label.jsx
+++ src/components/form/label/label.jsx
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types'
 import cn from 'classnames'
 
 import './label.css'
+import Tooltip from 'components/tooltip/tooltip'
 
 const Label = ({
-  name, label, className, children, error, classNameInput,
+  name, label, title, className, children, error, classNameInput,
 }) => (
   <div className={cn('form-label', { 'form-has-error': !!error }, className)}>
-    {label && <label htmlFor={name}>{label}</label>}
+    {label && (
+      <label htmlFor={name}>
+        {title ? <Tooltip tooltip={title} placement="bottom">{label}</Tooltip> : label}
+      </label>
+    )}
     <div className={classNameInput}>
       {children}
       {error && <div className="form-error">{error || ''}</div>}
@@ -18,6 +23,7 @@ const Label = ({
 
 Label.propTypes = {
   label: PropTypes.string,
+  title: PropTypes.string,
   children: PropTypes.node.isRequired,
   name: PropTypes.string,
   error: PropTypes.string,
@@ -27,6 +33,7 @@ Label.propTypes = {
 
 Label.defaultProps = {
   label: undefined,
+  title: undefined,
   name: undefined,
   error: undefined,
   className: undefined,
--- src/components/form/renderField.jsx
+++ src/components/form/renderField.jsx
@@ -9,9 +9,9 @@ import { DayPicker, DayRangePicker } from './dayPicker'
 import Label from './label'
 
 const renderField = component => ({
-  input, label, type, meta, placeholder, children, ...rest
+  input, label, title, type, meta, placeholder, children, ...rest
 }) => (
-  <Label name={input.name} label={label} error={meta.error}>
+  <Label name={input.name} label={label} title={title} error={meta.error}>
     {component === 'address' && <Address {...input} autoComplete="nope" />}
     {component === 'input' && <input {...input} id={input.name} type={type} placeholder={placeholder} />}
     {component === 'select' && <select {...input} id={input.name}>{children}</select>}
