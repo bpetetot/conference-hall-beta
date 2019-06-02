221bb4730e5b6b0826879a371586071a9bf25c7c
Benjamin Petetot <bpetetot@gmail.com>
2019-06-02 15:36:06 +0200

    :recycle: set email status if proposal exists

--- functions/email/emailWebhooks.js
+++ functions/email/emailWebhooks.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const functions = require('firebase-functions')
 const { getProposal, updateProposal } = require('../firestore/proposal')
 
@@ -5,8 +6,8 @@ module.exports.delivered = functions.https.onRequest(async (req, res) => {
   const userVariables = req.body['event-data']['user-variables']
   if (userVariables && userVariables.talkId && userVariables.eventId) {
     const proposal = await getProposal(userVariables.eventId, userVariables.talkId)
-    proposal.emailStatus = 'delivered'
     if (proposal) {
+      proposal.emailStatus = 'delivered'
       await updateProposal(userVariables.eventId, proposal)
       console.log(`talk with Id: ${proposal.id} marked as email delivered.`)
       return res.json({ result: `talk with Id: ${proposal.id} marked as email delivered.` })
