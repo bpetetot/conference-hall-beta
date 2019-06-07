2bd341e556f17e8c973879854dd4f1a3c5cc005e
Corinne Krych <corinnekrych@gmail.com>
2019-06-07 18:26:00 +0200

    :pencil: merge conflict in installation doc

--- functions/email/rsvp.js
+++ functions/email/rsvp.js
@@ -1,22 +1,35 @@
 const functions = require('firebase-functions')
-const { getProposal, updateProposal } = require('../firestore/proposal')
+const { fetchProposal } = require('../firestore/proposal')
+const { partialUpdateTalk } = require('../firestore/talk')
 
 module.exports.rsvp = functions.https.onRequest(async (req, res) => {
   const { eventId, talkId, value } = req.query
   if (eventId && talkId && value) {
-    const proposal = await getProposal(eventId, talkId)
+    const proposal = await fetchProposal(eventId, talkId)
     if (proposal) {
       if (value === 'yes') {
-        proposal.state = 'confirmed'
-        await updateProposal(eventId, proposal)
-        console.log(`talk with Id: ${proposal.id} is confirmed by speaker.`)
-        return res.json({ result: `talk with Id: ${proposal.id} is confirmed by speaker.` })
+        const newTalk = {
+          ...proposal,
+          submissions: {
+            [eventId]: {
+              state: 'confirmed',
+            },
+          },
+        }
+        partialUpdateTalk(talkId, newTalk)
+        return res.json({ result: `Talk with Id: ${newTalk.id} is confirmed by speaker.` })
       }
       if (value === 'no') {
-        proposal.state = 'declined'
-        await updateProposal(eventId, proposal)
-        console.log(`talk with Id: ${proposal.id} is confirmed by speaker.`)
-        return res.json({ result: `talk with Id: ${proposal.id} is declined by speaker.` })
+        const newTalk = {
+          ...proposal,
+          submissions: {
+            [eventId]: {
+              state: 'declined',
+            },
+          },
+        }
+        partialUpdateTalk(talkId, newTalk)
+        return res.json({ result: `talk with Id: ${newTalk.id} is declined by speaker.` })
       }
     }
     return res.json({ result: `talk with Id: ${talkId} not found.` })
--- functions/email/templates/talkAccepted.js
+++ functions/email/templates/talkAccepted.js
@@ -1,51 +1,23 @@
 module.exports = (event, users, talk, url) => `
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-  <title>✨Your talk has been accepted✨</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0 " />
+  <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "EmailMessage",
+  "potentialAction": {
+    "@type": "ConfirmAction",
+    "name": "Confirm presence",
+    "handler": {
+      "@type": "HttpActionHandler",
+      "url": "https://us-central1-rivieradev-db8f5.cloudfunctions.net/rsvp?eventId=${event.id}&amp;talkId=${talk.id}&amp;value=yes"
+    }
+  },
+  "description": "Reply to Conference-Hall for speaker presence to the event"
+}
+</script>
 </head>
 <body>
-
-<div itemscope itemtype="http://schema.org/Event">
-  <meta itemprop="name" content="Taco Night"/>
-  <meta itemprop="startDate" content="2027-04-18T15:30:00Z"/>
-  <meta itemprop="endDate" content="2027-04-18T16:30:00Z"/>
-  <div itemprop="location" itemscope itemtype="http://schema.org/Place">
-    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-      <meta itemprop="name" content="Google"/>
-      <meta itemprop="streetAddress" content="24 Willie Mays Plaza"/>
-      <meta itemprop="addressLocality" content="San Francisco"/>
-      <meta itemprop="addressRegion" content="CA"/>
-      <meta itemprop="postalCode" content="94107"/>
-      <meta itemprop="addressCountry" content="USA"/>
-    </div>
-  </div>
-  <div itemprop="potentialAction" itemscope itemtype="http://schema.org/RsvpAction">
-    <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-      <link itemprop="url" href="https://us-central1-rivieradev-db8f5.cloudfunctions.net/rsvp?eventId=${event.id}&amp;talkId=${talk.id}&amp;value=yes"/>
-    </div>
-    <link itemprop="attendance" href="http://schema.org/RsvpAttendance/Yes"/>
-    <meta itemprop="rsvpResponse" content="yes">
-  </div>
-  <div itemprop="potentialAction" itemscope itemtype="http://schema.org/RsvpAction">
-    <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-      <link itemprop="url" href="https://us-central1-rivieradev-db8f5.cloudfunctions.net/rsvp?eventId=${event.id}&amp;talkId=${talk.id}&amp;value=no"/>
-    </div>
-    <link itemprop="attendance" href="http://schema.org/RsvpAttendance/No"/>
-    <meta itemprop="rsvpResponse" content="no">
-  </div>
-  <div itemprop="potentialAction" itemscope itemtype="http://schema.org/RsvpAction">
-    <div itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-      <link itemprop="url" href="http://mysite.com/rsvp?eventId=123&value=maybe"/>
-    </div>
-    <link itemprop="attendance" href="http://schema.org/RsvpAttendance/Maybe"/>
-    <meta itemprop="rsvpResponse" content="maybe">
-  </div>
-</div>
-
   <p>Dear ${users.map(user => user.displayName).join(', ')}</p>
   <p>Your talk <strong>"${talk.title}"</strong> at <strong>"${event.name}"</strong> has been accepted! 🎉  🎊</p>
   <p>In order to help organizers for the selection and the event management, please confirm your participation:<p>
