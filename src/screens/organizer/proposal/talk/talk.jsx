5a32ef193f168fa67d23b1231756ad8956969bc1
Benjamin Petetot <bpetetot@gmail.com>
2019-07-15 22:53:49 +0200

    :sparkles: add status and date to proposal page (#612)

--- src/screens/organizer/proposal/talk/talk.jsx
+++ src/screens/organizer/proposal/talk/talk.jsx
@@ -2,37 +2,45 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
+import { withSizes } from 'styles/utils'
 import Badge from 'components/badge'
 import Markdown from 'components/markdown'
 import { FormatBadge, CategoryBadge } from 'screens/components/event/badges'
-import './talk.css'
+import TalkSelection from 'screens/organizer/components/talkSelection'
+import { formatTimestamp } from 'helpers/firebase'
 
-const Talk = ({ eventId, proposal, className }) => (
+import styles from './talk.module.css'
+
+const Talk = ({
+  eventId, proposal, deliberationActive, isMobile, className,
+}) => (
   <div className={cn(className, 'card')}>
-    <h2>{proposal.title} </h2>
-    <div className="proposal-talk-badges">
-      <FormatBadge
-        outline
-        pill
-        light
-        eventId={eventId}
-        formatId={proposal.formats}
-      />
-      <CategoryBadge
-        outline
-        pill
-        light
-        eventId={eventId}
-        categoryId={proposal.categories}
-      />
-      <Badge outline pill light>
-        {proposal.level}
-      </Badge>
-      <Badge outline pill light>
-        {proposal.language}
-      </Badge>
+    <div className={styles.title}>
+      <h2>{proposal.title}</h2>
+      <div>
+        {!isMobile && deliberationActive && (
+          <div>
+            <TalkSelection proposalId={proposal.id} />
+          </div>
+        )}
+      </div>
     </div>
-    <div className="proposal-talk-info">
+    <div className={styles.metadata}>
+      <div className={styles.badges}>
+        <FormatBadge outline pill light eventId={eventId} formatId={proposal.formats} />
+        <CategoryBadge outline pill light eventId={eventId} categoryId={proposal.categories} />
+        <Badge outline pill light>
+          {proposal.level}
+        </Badge>
+        <Badge outline pill light>
+          {proposal.language}
+        </Badge>
+      </div>
+      <div>
+        <small>Submitted on {formatTimestamp(proposal.createTimestamp)}</small>
+      </div>
+    </div>
+    <div className={styles.infos}>
       <Markdown source={proposal.abstract} />
       {proposal.references && <h3>References</h3>}
       <Markdown source={proposal.references} />
@@ -46,11 +54,15 @@ Talk.propTypes = {
   eventId: PropTypes.string.isRequired,
   proposal: PropTypes.objectOf(PropTypes.any),
   className: PropTypes.string,
+  deliberationActive: PropTypes.bool,
+  isMobile: PropTypes.bool,
 }
 
 Talk.defaultProps = {
   proposal: {},
+  deliberationActive: false,
+  isMobile: false,
   className: undefined,
 }
 
-export default Talk
+export default withSizes(Talk)
