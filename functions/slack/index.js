5fe6ca8bba5d4d3ef78bd623d33a182a8a571717
Benjamin Petetot <bpetetot@gmail.com>
2019-07-28 23:02:05 +0200

    :sparkles: slack integration

---
+++ functions/slack/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+const fetch = require('node-fetch')
+const { get, first } = require('lodash')
+
+const url = 'https://hooks.slack.com/services/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'
+
+const buildMessage = (event, talk, speakers, app) => ({
+  attachments: [
+    {
+      fallback: `New Talk submitted to ${event.name}`,
+      pretext: `*New talk submitted to ${event.name}*`,
+      author_name: `by ${speakers.map(s => s.displayName).join(' & ')}`,
+      title: talk.title,
+      text: talk.abstract,
+      title_link: `${app.url}/organizer/event/${event.id}/proposal/${talk.id}`,
+      thumb_url: get(first(speakers), 'photoURL'),
+      color: '#ffab00',
+    },
+  ],
+})
+
+const sendSlackMessage = async (event, talk, speakers, app) => {
+  const message = buildMessage(event, talk, speakers, app)
+  try {
+    console.info('send slack message', message)
+    const response = await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(message),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = {
+  sendSlackMessage,
+}
