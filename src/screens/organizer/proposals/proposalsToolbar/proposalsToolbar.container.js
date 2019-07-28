f0f2f1e98043adf13abc712fea84a9439c5b380d
Benjamin Petetot <bpetetot@gmail.com>
2019-07-28 20:17:20 +0200

    :sparkles: confirmation popin to send emails

--- src/screens/organizer/proposals/proposalsToolbar/proposalsToolbar.container.js
+++ src/screens/organizer/proposals/proposalsToolbar/proposalsToolbar.container.js
@@ -8,21 +8,21 @@ const mapStore = (store, props, { router }) => {
   const { exporting } = store.ui.organizer.proposalsExport.get()
   const { count } = store.ui.organizer.proposalsSelection.get()
   const totalProposals = store.data.proposals.getAsArray().length
-  const { items } = store.ui.organizer.proposalsSelection.get()
+  const { items: selection } = store.ui.organizer.proposalsSelection.get()
 
   return {
     deliberationActive,
     exporting,
     nbSelected: count,
     totalProposals,
-    selection: items,
-    onSelectAll: (isChecked) => {
-      store.dispatch({ type: '@@ui/SELECT_ALL_PROPOSALS', payload: { checkAll: isChecked } })
-    },
+    onSelectAll: e => store.dispatch({
+      type: '@@ui/SELECT_ALL_PROPOSALS',
+      payload: { checkAll: e.target.checked },
+    }),
     onExportProposals: output => () => store.dispatch({ type: '@@ui/EXPORT_PROPOSALS', payload: { output } }),
-    onSendEmails: selection => store.dispatch({ type: '@@ui/SEND_EMAIL_FOR_PROPOSALS', payload: { selection } }),
-    onAcceptProposals: selection => store.dispatch({ type: '@@ui/ACCEPT_PROPOSALS', payload: { selection } }),
-    onRejectProposals: selection => store.dispatch({ type: '@@ui/REJECT_PROPOSALS', payload: { selection } }),
+    onSendEmails: () => store.dispatch({ type: '@@ui/SEND_EMAIL_FOR_PROPOSALS', payload: { selection } }),
+    onAcceptProposals: () => store.dispatch({ type: '@@ui/ACCEPT_PROPOSALS', payload: { selection } }),
+    onRejectProposals: () => store.dispatch({ type: '@@ui/REJECT_PROPOSALS', payload: { selection } }),
   }
 }
 
--- src/screens/organizer/proposals/proposalsToolbar/proposalsToolbar.jsx
+++ src/screens/organizer/proposals/proposalsToolbar/proposalsToolbar.jsx
@@ -1,109 +1,85 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
-import isEmpty from 'lodash/isEmpty'
 
 import Checkbox from 'components/form/checkbox'
 import Button from 'components/button'
 import IconLabel from 'components/iconLabel'
 import Dropdown from 'components/dropdown'
 
+import { ConfirmationPopin } from 'components/portals'
 import styles from './proposalsToolbar.module.css'
 
-class ProposalToolbar extends Component {
-  state = {
-    checkAll: this.props.selection.length,
-  }
-
-  handleSelect = () => {
-    this.setState(
-      state => ({ checkAll: !state.checkAll }),
-      () => this.props.onSelectAll(this.state.checkAll),
-    )
-  }
-
-  render() {
-    const {
-      onSendEmails,
-      onExportProposals,
-      onAcceptProposals,
-      onRejectProposals,
-      selection,
-      deliberationActive,
-      exporting,
-      nbSelected,
-      totalProposals,
-    } = this.props
-
-    const { checkAll } = this.state
-
-    return (
-      <div className={cn(styles.proposalsActions, 'no-print')}>
-        <div className={styles.leftActions}>
-          <Checkbox
-            onChange={this.handleSelect}
-            label={!nbSelected ? `${totalProposals} proposals` : `${nbSelected} selected`}
-            name="all-pages"
-            value={checkAll}
-            disabled={!deliberationActive || totalProposals === 0}
-            indeterminate={nbSelected !== totalProposals}
-          />
-        </div>
-        <div className={styles.rightActions}>
-          {nbSelected === 0 && totalProposals > 0 && (
-            <Dropdown
-              action={(
-                <Button tertiary loading={!!exporting}>
-                  <IconLabel icon="fa fa-caret-down" label="Export..." right />
-                </Button>
+const ProposalToolbar = ({
+  onSelectAll,
+  onExportProposals,
+  onAcceptProposals,
+  onRejectProposals,
+  onSendEmails,
+  deliberationActive,
+  exporting,
+  nbSelected,
+  totalProposals,
+}) => (
+  <div className={cn(styles.proposalsActions, 'no-print')}>
+    <div className={styles.leftActions}>
+      <Checkbox
+        onChange={onSelectAll}
+        label={!nbSelected ? `${totalProposals} proposals` : `${nbSelected} selected`}
+        name="all-pages"
+        value={!!nbSelected}
+        disabled={!deliberationActive || totalProposals === 0}
+        indeterminate={nbSelected !== totalProposals}
+      />
+    </div>
+    <div className={styles.rightActions}>
+      {nbSelected === 0 && totalProposals > 0 && (
+        <Dropdown
+          action={(
+            <Button tertiary loading={!!exporting}>
+              <IconLabel icon="fa fa-caret-down" label="Export..." right />
+            </Button>
 )}
-            >
-              <Button onClick={onExportProposals('json')} disabled={!!exporting}>
-                <IconLabel icon="fa fa-cloud-download" label="JSON file" />
-              </Button>
-              <Button onClick={onExportProposals('pdf')} disabled={!!exporting}>
-                <IconLabel icon="fa fa-file-pdf-o" label="PDF cards" />
-              </Button>
-            </Dropdown>
-          )}
-          {deliberationActive && nbSelected > 0 && (
-            <Fragment>
-              <Button
-                tertiary
-                onClick={() => onAcceptProposals(selection)}
-                disabled={isEmpty(selection)}
-              >
-                <IconLabel icon="fa fa-check" label="Accept proposals" />
-              </Button>
-              <Button
-                tertiary
-                onClick={() => onRejectProposals(selection)}
-                disabled={isEmpty(selection)}
-              >
-                <IconLabel icon="fa fa-close" label="Reject proposals" />
-              </Button>
-              <Button
-                tertiary
-                onClick={() => onSendEmails(selection)}
-                disabled={isEmpty(selection)}
-              >
+        >
+          <Button onClick={onExportProposals('json')} disabled={!!exporting}>
+            <IconLabel icon="fa fa-cloud-download" label="JSON file" />
+          </Button>
+          <Button onClick={onExportProposals('pdf')} disabled={!!exporting}>
+            <IconLabel icon="fa fa-file-pdf-o" label="PDF cards" />
+          </Button>
+        </Dropdown>
+      )}
+      {deliberationActive && nbSelected > 0 && (
+        <Fragment>
+          <Button tertiary onClick={onAcceptProposals} disabled={nbSelected === 0}>
+            <IconLabel icon="fa fa-check" label="Accept proposals" />
+          </Button>
+          <Button tertiary onClick={onRejectProposals} disabled={nbSelected === 0}>
+            <IconLabel icon="fa fa-close" label="Reject proposals" />
+          </Button>
+          <ConfirmationPopin
+            title="Send emails to speakers"
+            content={`You are going to send about ${nbSelected} acceptation and/or rejection emails to speakers. Continue?`}
+            onOk={onSendEmails}
+            withCancel
+            renderTrigger={({ show }) => (
+              <Button tertiary onClick={show} disabled={nbSelected === 0}>
                 <IconLabel icon="fa fa-rocket" label="Send emails" />
               </Button>
-            </Fragment>
-          )}
-        </div>
-      </div>
-    )
-  }
-}
+            )}
+          />
+        </Fragment>
+      )}
+    </div>
+  </div>
+)
 
 ProposalToolbar.propTypes = {
-  selection: PropTypes.arrayOf(PropTypes.string),
-  onSendEmails: PropTypes.func.isRequired,
   onSelectAll: PropTypes.func.isRequired,
   onAcceptProposals: PropTypes.func.isRequired,
   onRejectProposals: PropTypes.func.isRequired,
   onExportProposals: PropTypes.func.isRequired,
+  onSendEmails: PropTypes.func.isRequired,
   deliberationActive: PropTypes.bool,
   exporting: PropTypes.string,
   nbSelected: PropTypes.number,
@@ -111,7 +87,6 @@ ProposalToolbar.propTypes = {
 }
 
 ProposalToolbar.defaultProps = {
-  selection: [],
   deliberationActive: false,
   exporting: null,
   nbSelected: 0,
