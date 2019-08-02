ef1cd50acbe63d75ce78a5f2dfa3a1ea98550753
Benjamin Petetot <bpetetot@gmail.com>
2019-08-02 14:41:59 +0200

    :lipstick: big style update

--- src/screens/speaker/event/submitWizard/talkSubmission/talkSubmission.jsx
+++ src/screens/speaker/event/submitWizard/talkSubmission/talkSubmission.jsx
@@ -78,7 +78,7 @@ const TalkSubmission = ({
           <Field
             name="comments"
             label="Message to organizers"
-            tooltip="Ask special requirements to organizers or just thanks them."
+            hints="Ask special requirements to organizers or just thanks them."
             component={markdownInput}
           />
         </div>
