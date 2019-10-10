066aa2bd082e0a13bf1f8989cf310baf46787128
Gerard Paligot <gerard.paligot@gmail.com>
2019-10-10 17:41:45 +0200

    :ok_hand: Move field between fullname and company and required.

--- src/screens/components/profile/profile.jsx
+++ src/screens/components/profile/profile.jsx
@@ -34,8 +34,8 @@ const Profile = ({
             validate={required}
             inline
           />
+          <Field name="email" label="Email" type="text" component={input} validate={required} inline />
           <Field name="company" label="Company" type="text" component={input} inline />
-          <Field name="email" label="Email" type="text" component={input} inline />
           <Field name="phone" label="Phone" type="text" component={input} inline />
           <Field name="language" label="Spoken language" type="text" component={input} inline />
           <Field
