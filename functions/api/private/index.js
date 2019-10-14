6850d475e10e3eebfbb6126a2eebb581260d27f5
Bruno Sabot <brunosabot@gmail.com>
2019-10-14 17:44:12 +0200

    :ok_hand: Move accept and reject proposal apis to v1

--- functions/api/private/index.js
+++ functions/api/private/index.js
@@ -3,8 +3,6 @@ const cors = require('cors')({ origin: true })
 
 const withAuth = require('../middlewares/auth')
 const exportEvent = require('./exportEvent')
-const acceptProposal = require('./proposal/accept')
-const rejectProposal = require('./proposal/reject')
 
 // router
 const router = express.Router()
@@ -14,9 +12,5 @@ router.use(withAuth)
 
 // Export proposals
 router.get('/export/:eventId', exportEvent)
-
-// Proposal deliberation API
-router.get('/proposal/:eventId/:proposalId/accept', acceptProposal)
-router.get('/proposal/:eventId/:proposalId/reject', rejectProposal)
 
 module.exports = router
