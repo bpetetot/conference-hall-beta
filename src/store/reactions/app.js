d82d363700b3526ae6b7281bad6c75bfb4125873
Benjamin Petetot <bpetetot@gmail.com>
2019-12-20 16:44:32 +0100

    :bug: fix a bug avoiding event page reload

--- src/store/reactions/app.js
+++ src/store/reactions/app.js
@@ -1,11 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
-export const init = (action, store, { router }) => {
-  const isOrganizer = router.getParam('root') === 'organizer'
-
+export const init = (action, store) => {
   // get eventId from app event context
   const currentEventId = localStorage.getItem('currentEventId')
-  if (!isOrganizer && currentEventId) {
+  if (currentEventId) {
     store.ui.app.update({ currentEventId })
     store.dispatch({ type: '@@ui/ON_LOAD_EVENT', payload: currentEventId })
   }
