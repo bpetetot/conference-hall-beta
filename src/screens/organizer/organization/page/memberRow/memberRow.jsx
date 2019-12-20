5846fd0b9feff3a82aacea8d4e692cca24377fdf
Benjamin Petetot <bpetetot@gmail.com>
2019-12-20 16:47:41 +0100

    :recycle: adapt organization component with the new model

--- src/screens/organizer/organization/page/memberRow/memberRow.jsx
+++ src/screens/organizer/organization/page/memberRow/memberRow.jsx
@@ -2,36 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import IconLabel from 'components/iconLabel'
-import RelativeDate from 'components/relativeDate'
 import Button from 'components/button'
 import { ListItem } from 'components/list'
 import { ConfirmationPopin } from 'components/portals'
 import Avatar from 'components/avatar/avatar'
 
-import { toDate } from 'helpers/firebase'
-
 import './memberRow.css'
 
-const MemberRow = ({
-  uid,
-  displayName,
-  photoURL,
-  updateTimestamp,
-  owner,
-  removeMember,
-  authUserId,
-}) => {
+const MemberRow = ({ uid, displayName, photoURL, role, isOwner, removeMember, authUserId }) => {
   if (!displayName) return null
 
-  const canRemove = owner === authUserId && owner !== uid
-  const canLeave = owner !== authUserId && authUserId === uid
+  const canRemove = isOwner && authUserId !== uid
+  const canLeave = authUserId === uid
 
   return (
     <>
       <ListItem
         key={uid}
-        title={<Avatar name={displayName} src={photoURL} withLabel />}
-        subtitle={<RelativeDate date={toDate(updateTimestamp)} />}
+        title={
+          <>
+            <Avatar name={displayName} src={photoURL} withLabel />
+            {role}
+          </>
+        }
         renderActions={() => (
           <ConfirmationPopin
             title={canRemove ? 'Remove a member' : 'Leave organization'}
@@ -66,8 +59,8 @@ MemberRow.propTypes = {
   uid: PropTypes.string.isRequired,
   displayName: PropTypes.string,
   photoURL: PropTypes.string,
-  updateTimestamp: PropTypes.any,
-  owner: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  isOwner: PropTypes.bool.isRequired,
   removeMember: PropTypes.func.isRequired,
   authUserId: PropTypes.string.isRequired,
 }
@@ -75,7 +68,6 @@ MemberRow.propTypes = {
 MemberRow.defaultProps = {
   displayName: undefined,
   photoURL: undefined,
-  updateTimestamp: undefined,
 }
 
 export default MemberRow
--- src/screens/organizer/organization/page/organizationPage.jsx
+++ src/screens/organizer/organization/page/organizationPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from '@k-redux-router/react-k-ramel'
-import keys from 'lodash/keys'
 
 import Titlebar from 'components/titlebar'
 import { List } from 'components/list'
 import IconLabel from 'components/iconLabel'
 import Button from 'components/button'
+import HasRole from 'screens/components/hasRole'
 
 import AddMember from './addMember'
 import MemberRow from './memberRow'
@@ -18,46 +18,54 @@ const OrganizationPage = ({
   members,
   onSelectUser,
   removeMember,
-  owner,
   authUserId,
-}) => (
-  <div className="organization-page">
-    <Titlebar className="organization-header" icon="fa fa-users" title={name}>
-      <Button secondary>
-        {btn => (
-          <Link code="organizer-organization-edit" organizationId={organizationId} className={btn}>
-            <IconLabel icon="fa fa-pencil" label="Edit" />
-          </Link>
+}) => {
+  const isOwner = members[authUserId] === 'owner'
+  return (
+    <div className="organization-page">
+      <Titlebar className="organization-header" icon="fa fa-users" title={name}>
+        <HasRole of={['owner']} forOrganizationId={organizationId}>
+          <Button secondary>
+            {btn => (
+              <Link
+                code="organizer-organization-edit"
+                organizationId={organizationId}
+                className={btn}
+              >
+                <IconLabel icon="fa fa-pencil" label="Edit" />
+              </Link>
+            )}
+          </Button>
+          <AddMember
+            organizationId={organizationId}
+            organizationName={name}
+            onSelectUser={onSelectUser}
+          />
+        </HasRole>
+      </Titlebar>
+      <List
+        className="organization-content"
+        array={Object.entries(members)}
+        noResult="No users yet !"
+        renderRow={([uid, role]) => (
+          <MemberRow
+            key={uid}
+            uid={uid}
+            role={role}
+            authUserId={authUserId}
+            removeMember={() => removeMember(uid)}
+            isOwner={isOwner}
+          />
         )}
-      </Button>
-      <AddMember
-        organizationId={organizationId}
-        organizationName={name}
-        onSelectUser={onSelectUser}
       />
-    </Titlebar>
-    <List
-      className="organization-content"
-      array={keys(members)}
-      noResult="No users yet !"
-      renderRow={uid => (
-        <MemberRow
-          key={uid}
-          uid={uid}
-          authUserId={authUserId}
-          removeMember={() => removeMember(uid)}
-          owner={owner}
-        />
-      )}
-    />
-  </div>
-)
+    </div>
+  )
+}
 
 OrganizationPage.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  members: PropTypes.objectOf(PropTypes.bool),
-  owner: PropTypes.string.isRequired,
+  members: PropTypes.objectOf(PropTypes.string),
   authUserId: PropTypes.string.isRequired,
   onSelectUser: PropTypes.func.isRequired,
   removeMember: PropTypes.func.isRequired,
