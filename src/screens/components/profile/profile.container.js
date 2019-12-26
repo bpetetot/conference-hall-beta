ba4368b00433feb16595e9dae9e99c5d94c1f324
Benjamin Petetot <bpetetot@gmail.com>
2019-12-26 19:22:28 +0100

    :sparkles: change photo url and reset in profile form (#745)

--- src/screens/components/profile/profile.container.js
+++ src/screens/components/profile/profile.container.js
@@ -1,6 +1,8 @@
+import firebase from 'firebase/app'
 import { compose } from 'redux'
 import { inject } from '@k-ramel/react'
 import { forRoute } from '@k-redux-router/react-k-ramel'
+import pick from 'lodash/pick'
 
 import Profile from './profile'
 
@@ -11,6 +13,10 @@ const mapStore = store => {
     ...user,
     initialValues: user,
     submitting: store.ui.loaders.get().isProfileSaving,
+    setDefault: () => {
+      const payload = pick(firebase.auth().currentUser, ['uid', 'email', 'displayName', 'photoURL'])
+      store.dispatch({ type: '@@ui/SAVE_PROFILE', payload })
+    },
     onSubmit: payload => {
       store.dispatch({ type: '@@ui/SAVE_PROFILE', payload })
     },
