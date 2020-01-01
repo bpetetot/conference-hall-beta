7fdd60d9856015d2707b671718e3a5c0bfe916a4
Benjamin Petetot <bpetetot@gmail.com>
2020-01-01 15:07:54 +0100

    :sparkles: hide sorting by rating in proposal list, when enabled in event config

--- src/screens/organizer/proposals/proposalsFilters/proposalsFilters.container.js
+++ src/screens/organizer/proposals/proposalsFilters/proposalsFilters.container.js
@@ -3,6 +3,10 @@ import get from 'lodash/get'
 
 import ProposalFilters from './proposalsFilters'
 
+const filterSortOrders = (sortOrders, hideRatings) => {
+  return sortOrders.filter(order => !(/Rating/gm.test(order) && hideRatings))
+}
+
 const mapStore = (store, props, { router }) => {
   const eventId = router.getParam('eventId')
   const sortOrders = router.getParam('sortOrders')
@@ -11,6 +15,7 @@ const mapStore = (store, props, { router }) => {
   const { formats, categories } = store.data.events.get(eventId) || {}
   const settings = store.data.eventsSettings.get(eventId)
   const filters = store.ui.organizer.proposals.get()
+  const hideRatings = get(settings, 'deliberation.hideRatings')
 
   return {
     eventId,
@@ -18,8 +23,8 @@ const mapStore = (store, props, { router }) => {
     ratings,
     formats,
     categories,
-    sortOrders,
     filters,
+    sortOrders: filterSortOrders(sortOrders, hideRatings),
     deliberationActive: get(settings, 'deliberation.enabled'),
     onChange: ({ target }) => {
       store.ui.organizer.proposals.update({ [target.id]: target.value })
