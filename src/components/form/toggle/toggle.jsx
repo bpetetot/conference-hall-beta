9a3b4d0c170edc7e953dd65c6da4fcbb189e4083
Benjamin Petetot <bpetetot@gmail.com>
2020-01-01 16:53:21 +0100

    :bug: fix the visibility toggle in event form (#750)

--- src/components/form/toggle/toggle.jsx
+++ src/components/form/toggle/toggle.jsx
@@ -1,82 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 
-import './toggle.css'
+import styles from './toggle.module.css'
 
-class Toggle extends Component {
-  state = {
-    checked: this.props.checked,
-  }
+const Toggle = ({ name, checked, onChange, ...rest }) => {
+  const [isChecked, setChecked] = useState(checked)
+  const handleChange = useCallback(
+    e => {
+      setChecked(e.target.checked)
+      onChange(e.target.checked)
+    },
+    [onChange],
+  )
 
-  getValue = checked => {
-    const { truthy, falsy } = this.props
-    if (checked && truthy) {
-      return truthy
-    }
-    if (!checked && falsy) {
-      return falsy
-    }
-    return checked
-  }
-
-  getChecked = value => {
-    const { truthy, falsy } = this.props
-    const { checked } = this.state
-    if (!truthy && !falsy) {
-      return checked
-    }
-    if (value === truthy) {
-      return true
-    }
-    if (value === falsy) {
-      return false
-    }
-    return value
-  }
-
-  handleChange = e => {
-    const { checked } = e.target
-    const { onChange } = this.props
-    this.setState(
-      () => ({ checked }),
-      () => onChange(this.getValue(checked)),
-    )
-  }
-
-  render() {
-    const { name, value, ...rest } = this.props
-    const checked = this.getChecked(value)
-
-    return (
-      <label className="toggle" htmlFor={name}>
-        <input
-          id={name}
-          name={name}
-          type="checkbox"
-          {...rest}
-          checked={checked}
-          onChange={this.handleChange}
-        />
-        <span className="toggle-item" />
-      </label>
-    )
-  }
+  return (
+    <label className={styles.toggle} htmlFor={name}>
+      <input id={name} type="checkbox" {...rest} checked={isChecked} onChange={handleChange} />
+      <span className={styles.item} />
+    </label>
+  )
 }
 
 Toggle.propTypes = {
   name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  truthy: PropTypes.string,
-  falsy: PropTypes.string,
   checked: PropTypes.bool,
-  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
 }
 
 Toggle.defaultProps = {
-  truthy: undefined,
-  falsy: undefined,
   checked: false,
-  value: undefined,
 }
 
 export default Toggle
