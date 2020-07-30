67f58b164900ab733f9289f02a4150aeccb3d51c
Benjamin Petetot <bpetetot@gmail.com>
2020-07-30 18:41:50 +0200

    Use auth hook in login

--- src/screens/conference/login/login.jsx
+++ src/screens/conference/login/login.jsx
@@ -1,56 +1,66 @@
-import firebase from 'firebase/app'
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import firebase from 'firebase/app'
+import { forRoute } from '@k-redux-router/react-k-ramel'
+import { inject } from '@k-ramel/react'
+import split from 'lodash/split'
 
+import { useAuth } from 'features/auth'
 import { LoadingIndicator } from 'components/loader'
 import IconLabel from 'components/iconLabel'
 import Button from 'components/button'
 import './login.css'
 
-const Login = ({ authenticated, providers, signin }) => {
-  const [authenticating, setAuthenticating] = useState(false)
-  const [errorMessage, setErrorMessage] = useState()
+const PROVIDERS = split(process.env.REACT_APP_AUTH_PROVIDERS, ',') || []
+
+const Login = ({ redirectNext }) => {
+  const [redirecting, setRedirecting] = useState(false)
+  const [error, setError] = useState()
+  const { user, loading, signin } = useAuth()
 
   useEffect(() => {
-    setAuthenticating(true)
+    if (user && !redirecting) redirectNext()
+  }, [user, redirecting, redirectNext])
+
+  useEffect(() => {
+    setRedirecting(true)
     firebase
       .auth()
       .getRedirectResult()
-      .then(() => {
-        setAuthenticating(false)
-      })
-      .catch((error) => {
-        setAuthenticating(false)
-        setErrorMessage(error.message)
+      .then(() => setRedirecting(false))
+      .catch((e) => {
+        setRedirecting(false)
+        setError(e.message)
       })
   }, [])
 
-  if (authenticating || authenticated) {
+  if (redirecting || loading) {
     return <LoadingIndicator className="login-loading" />
   }
 
   return (
     <div className="login">
       <h1 className="login-title">Connexion</h1>
-      {providers.map((provider) => (
+      {PROVIDERS.map((provider) => (
         <Button key={provider} className={`btn-${provider}`} onClick={() => signin(provider)}>
           <IconLabel icon={`fa fa-${provider}`} label={`with ${provider}`} />
         </Button>
       ))}
-      {errorMessage && <p className="login-error">{errorMessage}</p>}
+      {error && <p className="login-error">{error}</p>}
     </div>
   )
 }
 
 Login.propTypes = {
-  authenticated: PropTypes.bool,
-  providers: PropTypes.arrayOf(PropTypes.string),
-  signin: PropTypes.func.isRequired,
+  redirectNext: PropTypes.func.isRequired,
 }
 
-Login.defaultProps = {
-  providers: [],
-  authenticated: false,
-}
+const LoginContainer = inject((store) => {
+  return {
+    redirectNext: () => {
+      store.dispatch('@@router/REDIRECT_TO_NEXT_URL')
+    },
+  }
+})(Login)
 
-export default Login
+export default forRoute.absolute('login')(LoginContainer)
