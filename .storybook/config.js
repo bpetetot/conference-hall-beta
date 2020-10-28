da30f85f9f98743f4d988eb09863d6a798203f0d
Benjamin Petetot <bpetetot@gmail.com>
2020-10-28 23:55:08 +0100

    Fix tests and storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { configure, addDecorator, addParameters } from '@storybook/react'
 
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 
 import 'normalize.css'
 import 'font-awesome/css/font-awesome.min.css'
@@ -8,13 +10,19 @@ import 'styles'
 
 import theme from './theme'
 
+const history = createMemoryHistory()
+
 addParameters({
   options: {
     theme,
   },
 })
 
-addDecorator(story => <div className="default-theme">{story()}</div>)
+addDecorator((story) => (
+  <MemoryRouter history={history}>
+    <div className="default-theme">{story()}</div>
+  </MemoryRouter>
+))
 
 configure(
   [
