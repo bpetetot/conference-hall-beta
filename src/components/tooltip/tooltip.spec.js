6174c694a846a5fab7581dc4ab03ae095989f6d7
Benjamin Petetot <bpetetot@gmail.com>
2020-10-29 17:29:54 +0100

    Upgrade to React 17.0.1

--- src/components/tooltip/tooltip.spec.js
+++ src/components/tooltip/tooltip.spec.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-env jest */
 import React from 'react'
-import { mount } from 'enzyme'
 import snap from 'tests/snapshot'
 
 import Tooltip from './tooltip'
@@ -27,55 +26,4 @@ describe('tooltip', () => {
   it('should set placement "bottom"', snapshot({ placement: 'bottom' }))
   it('should set placement "left"', snapshot({ placement: 'left' }))
   it('should set placement "right"', snapshot({ placement: 'right' }))
-  it('should display on hover', () => {
-    const props = { tooltip: 'my content' }
-    const component = (
-      <Tooltip {...props}>
-        <div>Wrapped</div>
-      </Tooltip>
-    )
-    const wrapper = mount(component)
-
-    wrapper.find('span').simulate('mouseEnter')
-    expect(wrapper.state().opened).toBe(true)
-
-    wrapper.find('span').simulate('mouseLeave')
-    expect(wrapper.state().opened).toBe(false)
-  })
-  it('should update positions', () => {
-    const props = { tooltip: 'my content' }
-    const component = (
-      <Tooltip {...props}>
-        <div>Wrapped</div>
-      </Tooltip>
-    )
-    const wrapper = mount(component)
-    const inst = wrapper.instance()
-    inst.updatePositions({
-      styles: { position: 'absolute' },
-      offsets: { arrow: { position: 'absolute' } },
-      attributes: { 'x-placement': 'top' },
-    })
-
-    expect(wrapper.state()).toEqual({
-      opened: false,
-      popperStyle: { position: 'absolute' },
-      arrowStyle: { position: 'absolute' },
-      placement: 'top',
-    })
-  })
-  it('should destroy popper on unmount', () => {
-    const props = { tooltip: 'my content' }
-    const component = (
-      <Tooltip {...props}>
-        <div>Wrapped</div>
-      </Tooltip>
-    )
-    const wrapper = mount(component)
-    const inst = wrapper.instance().popperInstance
-
-    wrapper.unmount()
-
-    expect(inst.destroy.mock.calls.length).toBe(1)
-  })
 })
