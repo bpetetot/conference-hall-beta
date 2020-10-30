243649645d7b11ee63ec7d6f4905d8e6716ba788
Benjamin Petetot <bpetetot@gmail.com>
2020-10-31 00:51:25 +0100

    use hook in hasRole form

--- src/features/organization/hasRole/hasRole.container.js
+++ src/features/organization/hasRole/hasRole.container.js
@@ -2,13 +2,11 @@ import { inject } from '@k-ramel/react'
 
 import HasRole from './hasRole'
 
-const mapStore = (store, { forEventId, forOrganizationId }) => {
+const mapStore = (store, { forEventId }) => {
   const event = store.data.events.get(forEventId)
-  const organization = store.data.organizations.get()[event?.organization ?? forOrganizationId]
-
   return {
     eventOwner: event?.owner,
-    orgaMembers: organization?.members,
+    eventOrganizationId: event?.organization,
   }
 }
 
