7db93dccfd766b1300162c5a1f94785e84f72290
Benjamin Petetot <bpetetot@gmail.com>
2020-11-02 20:00:10 +0100

    :recycle: move Profile to auth feature

--- src/app/speaker/index.jsx
+++ src/app/speaker/index.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Profile from 'features/profile'
+import Profile from 'features/auth/profile'
 import Contributors from 'features/contributors'
 import Talks from 'features/talk/list'
 import Talk from './talk'
