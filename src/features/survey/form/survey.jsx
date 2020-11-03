3e1fdd5b109d87023f509b4b0ef40c50bdb5b4a9
Benjamin Petetot <bpetetot@gmail.com>
2020-11-03 12:15:46 +0100

    :recycle: get event with hook in survey form

--- src/features/survey/form/survey.jsx
+++ src/features/survey/form/survey.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Form } from 'react-final-form'
 
 import { SubmitButton } from 'components/form'
+import { LoadingIndicator } from 'components/loader'
 import { useAuth } from 'features/auth'
 import EventTitle from 'features/event/eventTitle'
+import { useCurrentEvent } from 'features/event/currentEventContext'
 import SurveyInput from './surveyInput'
 import { useSaveSurvey, useSurvey } from '../useSurveys'
 import questions from '../questions'
 
 import './survey.css'
 
-const SpeakerSurvey = ({ eventId, survey, name }) => {
+const SurveyForm = () => {
   const { user } = useAuth()
-  const { data } = useSurvey(eventId, user.uid)
-  const [saveSurvey] = useSaveSurvey(eventId, user.uid)
+  const { data: event, isLoading: isEventLoading } = useCurrentEvent()
+  const { data: userSurvey, isLoading: isSurveyLoading } = useSurvey(event.id, user.uid)
+  const [saveSurvey] = useSaveSurvey(event.id, user.uid)
+
+  if (isEventLoading || isSurveyLoading) return <LoadingIndicator />
 
   return (
     <div className="speaker-survey">
-      <EventTitle name={name} subtitle="Speaker survey" />
+      <EventTitle name={event.name} subtitle="Speaker survey" />
       <div className="card">
         <p>
           Organizers need some information about you in order to make a better event experience for
           speakers. Please fill the following survey to help them.
         </p>
-        <Form onSubmit={saveSurvey} initialValues={data}>
+        <Form onSubmit={saveSurvey} initialValues={userSurvey}>
           {({ handleSubmit, pristine, submitting }) => (
             <form>
-              {questions.map((q) => survey[q.name] && <SurveyInput key={q.name} {...q} />)}
+              {questions.map((q) => event.survey[q.name] && <SurveyInput key={q.name} {...q} />)}
               <SubmitButton handleSubmit={handleSubmit} pristine={pristine} submitting={submitting}>
                 Save Survey
               </SubmitButton>
@@ -39,14 +43,4 @@ const SpeakerSurvey = ({ eventId, survey, name }) => {
   )
 }
 
-SpeakerSurvey.propTypes = {
-  eventId: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  survey: PropTypes.objectOf(PropTypes.bool),
-}
-
-SpeakerSurvey.defaultProps = {
-  survey: {},
-}
-
-export default SpeakerSurvey
+export default SurveyForm
