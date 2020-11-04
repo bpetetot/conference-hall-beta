8c5b2fea1fa69ae65efa560ea26b4d0cc32d84e0
Benjamin Petetot <bpetetot@gmail.com>
2020-11-04 16:54:26 +0100

    use hook for event survey edit

--- src/features/event/edit/survey/survey.jsx
+++ src/features/event/edit/survey/survey.jsx
@@ -8,37 +8,38 @@ import Checkbox from 'components/form/checkbox'
 import questions from 'features/survey/questions'
 
 import './survey.css'
+import { useEvent, useSaveEvent } from 'features/event/useEvents'
 
-const SurveyForm = ({ surveyActive, survey, onActiveSurvey, onSelectQuestion }) => (
-  <div className="survey-form card">
-    <Label name="surveyActive" label="Enable Survey">
-      <Toggle name="surveyActive" checked={surveyActive} onChange={onActiveSurvey} />
-    </Label>
-    <h4>Select questions that you want to ask to speakers :</h4>
-    {questions.map((question) => (
-      <Checkbox
-        key={question.name}
-        name={question.name}
-        label={question.label}
-        info={question.organizerInfo}
-        onChange={onSelectQuestion}
-        value={survey[question.name]}
-        disabled={!surveyActive}
-      />
-    ))}
-  </div>
-)
+const SurveyForm = ({ eventId }) => {
+  const { data: event } = useEvent(eventId)
+  const [saveEvent] = useSaveEvent(eventId)
 
-SurveyForm.propTypes = {
-  surveyActive: PropTypes.bool,
-  survey: PropTypes.objectOf(PropTypes.bool),
-  onActiveSurvey: PropTypes.func.isRequired,
-  onSelectQuestion: PropTypes.func.isRequired,
+  const onActiveSurvey = (checked) => saveEvent({ surveyActive: checked })
+  const onSelectQuestion = ({ target }) => saveEvent({ [`survey.${target.name}`]: target.checked })
+
+  return (
+    <div className="survey-form card">
+      <Label name="surveyActive" label="Enable Survey">
+        <Toggle name="surveyActive" checked={event?.surveyActive} onChange={onActiveSurvey} />
+      </Label>
+      <h4>Select questions that you want to ask to speakers :</h4>
+      {questions.map((question) => (
+        <Checkbox
+          key={question.name}
+          name={question.name}
+          label={question.label}
+          info={question.organizerInfo}
+          onChange={onSelectQuestion}
+          value={event?.survey?.[question.name]}
+          disabled={!event?.surveyActive}
+        />
+      ))}
+    </div>
+  )
 }
 
-SurveyForm.defaultProps = {
-  surveyActive: false,
-  survey: {},
+SurveyForm.propTypes = {
+  eventId: PropTypes.string.isRequired,
 }
 
 export default SurveyForm
--- src/store/listeners.js
+++ src/store/listeners.js
@@ -7,13 +7,6 @@ export default [
   /* events */
   when('@@ui/ON_LOAD_ORGANIZER_EVENTS')(events.fetchOrganizerEvents),
   when('@@ui/ON_LOAD_SPEAKER_EVENTS')(events.fetchSpeakerEvents),
-
-  when('@@ui/ON_TOGGLE_EVENT_SURVEY')(events.updateEvent),
-  when('@@ui/ON_CHANGE_EMAIL_DESTINATION')(events.updateEvent),
-  when('@@ui/ON_CHANGE_EMAIL_NOTIFICATION')(events.updateEvent),
-  when('@@ui/ON_SELECT_SURVEY_QUESTION')(events.updateEvent),
-  when('@@ui/ON_TOGGLE_EVENT_DELIBERATION')(events.updateEvent),
-  when('@@ui/ON_TOGGLE_EVENT_DISPLAY_ORGANIZERS_RATINGS')(events.updateEvent),
   when('@@ui/ON_ORGANIZER_CHANGE_EVENT')(events.organizerChangeEvent),
   when('@@ui/ON_SAVE_EVENT_SETTINGS')(events.saveEventSettings),
   /* proposals */
