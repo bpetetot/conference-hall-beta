ff69115d2f71437a66ac3b9739432c07b4c1d370
Benjamin Petetot <bpetetot@gmail.com>
2021-04-15 13:55:43 +0200

    :sparkles: First version of the profile page

---
+++ webapp-next/pages/profile.tsx
@@ -0,0 +1,35 @@
+import { UserCircleIcon } from '@heroicons/react/outline'
+import { AcademicCapIcon } from '@heroicons/react/outline'
+import { AnnotationIcon } from '@heroicons/react/outline'
+
+import NavList from '../components/atoms/NavList'
+import Page from '../components/layout/Page'
+import PersonalInfoForm from '../components/user/PersonalInfoForm'
+import ProfileOverview from '../components/user/ProfileOverview'
+
+const ProfilePage = () => {
+  return (
+    <Page authenticated>
+      <ProfileOverview />
+      <div className="mt-4 lg:grid lg:grid-cols-12 lg:gap-x-5">
+        <NavList className="lg:col-span-3">
+          <NavList.Link href="#personal" icon={UserCircleIcon} defaultSelected>
+            Personal information
+          </NavList.Link>
+          <NavList.Link href="#speaker" icon={AcademicCapIcon}>
+            Speaker details
+          </NavList.Link>
+          <NavList.Link href="#additional" icon={AnnotationIcon}>
+            Additional information
+          </NavList.Link>
+        </NavList>
+
+        <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
+          <PersonalInfoForm />
+        </div>
+      </div>
+    </Page>
+  )
+}
+
+export default ProfilePage
---
+++ webapp-next/tests/pages/profile.spec.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react'
+
+import { User } from '../../api/user'
+import { useAuth } from '../../lib/auth'
+import Profile from '../../pages/profile'
+
+describe('Profile page', () => {
+  beforeEach(() => {
+    const user: User = { id: 1, uid: 'uid1', name: 'Bobby', photoURL: 'avatar' }
+    mockUseAuth.mockReturnValue({ user, isLoading: false, isAuthenticated: true })
+  })
+
+  test('should have loading if not authenticated', () => {
+    mockUseAuth.mockReturnValue({ isLoading: true, isAuthenticated: false })
+    render(<Profile />)
+    expect(screen.getByAltText('Loading...')).toBeVisible()
+  })
+
+  test('should render profile overview information', () => {
+    render(<Profile />)
+    expect(screen.getByText('Bobby')).toBeVisible()
+    expect(screen.getByRole('heading', { name: 'Profile Overview' })).toBeVisible()
+  })
+
+  test('should render personal information menu and form', () => {
+    render(<Profile />)
+    expect(screen.getByRole('link', { name: 'Personal information' })).toBeVisible()
+    expect(screen.getByRole('heading', { name: 'Personal information' })).toBeVisible()
+  })
+
+  test('should render speaker details menu and form', () => {
+    render(<Profile />)
+    expect(screen.getByRole('link', { name: 'Speaker details' })).toBeVisible()
+  })
+
+  test('should render additional information menu and form', () => {
+    render(<Profile />)
+    expect(screen.getByRole('link', { name: 'Additional information' })).toBeVisible()
+  })
+})
+
+// Mocks
+const mockUseAuth = useAuth as jest.Mock
+jest.mock('../../lib/auth', () => ({
+  useAuth: jest.fn(),
+}))
+jest.mock('next/router', () => ({
+  useRouter: () => ({ asPath: '/profile' }),
+}))
