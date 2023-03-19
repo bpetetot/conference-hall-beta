ed23b18bcd74aea44bbd7315ed0af7cf339c8cc1
Benjamin Petetot <bpetetot@gmail.com>
2023-03-19 19:58:37 +0100

    Typo

--- src/components/markdown/markdown.jsx
+++ src/components/markdown/markdown.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import cn from 'classnames';
-import { marked } from 'marked';
-import DOMPurify from 'dompurify';
+import React from 'react'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+import { marked } from 'marked'
+import DOMPurify from 'dompurify'
 
-import './markdown.css';
+import './markdown.css'
 
 marked.use({
   renderer: {
     link(href, title, text) {
-      const link = marked.Renderer.prototype.link.call(this, href, title, text);
-      return link.replace('<a', "<a target='_blank' ");
+      const link = marked.Renderer.prototype.link.call(this, href, title, text)
+      return link.replace('<a', "<a target='_blank' ")
     },
   },
-});
+})
 
 function Markdown({ source, className }) {
-  const html = marked.parse(source || '');
-  const safeHtml = DOMPurify.sanitize(html, { ADD_ATTR: ['target'] });
+  const html = marked.parse(source || '')
+  const safeHtml = DOMPurify.sanitize(html, { ADD_ATTR: ['target'] })
   return (
     <div
       className={cn('markdown', className)}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: safeHtml }}
     />
-  );
+  )
 }
 
 Markdown.propTypes = {
   source: PropTypes.string,
   className: PropTypes.string,
-};
+}
 
 Markdown.defaultProps = {
   source: '',
   className: undefined,
-};
+}
 
-export default Markdown;
+export default Markdown
