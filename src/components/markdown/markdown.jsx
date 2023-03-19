9a690db86c04d5adf0f2b238b6208079f7bade3e
Mathias Flagey <61873164+mflagey@users.noreply.github.com>
2023-03-19 20:03:02 +0100

    :bug: open links in a new tab by default in markdown renderer (#956)

--- src/components/markdown/markdown.jsx
+++ src/components/markdown/markdown.jsx
@@ -6,9 +6,18 @@ import DOMPurify from 'dompurify'
 
 import './markdown.css'
 
+marked.use({
+  renderer: {
+    link(href, title, text) {
+      const link = marked.Renderer.prototype.link.call(this, href, title, text)
+      return link.replace('<a', "<a target='_blank' ")
+    },
+  },
+})
+
 function Markdown({ source, className }) {
   const html = marked.parse(source || '')
-  const safeHtml = DOMPurify.sanitize(html)
+  const safeHtml = DOMPurify.sanitize(html, { ADD_ATTR: ['target'] })
   return (
     <div
       className={cn('markdown', className)}
